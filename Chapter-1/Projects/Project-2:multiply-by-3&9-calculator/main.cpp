b90358c38936cbd28804a33103a5b56aff48f286
JKBoy2244 <jobayer2006@gmail.com>
2026-07-27 20:50:29 +0100

    Update and rename main.cpp to main.cpp

--- Chapter-1/Projects/Project-2:multiply-by-3&9-calculator/main.cpp
+++ Chapter-1/Projects/Project-2:multiply-by-3&9-calculator/main.cpp
@@ -1,16 +1,5 @@
 #include <iostream> //Extremely important for std::cout and std::cin as without this line, the compiler won't know what those 2 commands mean
 
-int nine() 
-{
-  return 9;  //This function returns 9 so essentially 9 is stored in the function
-}
-
-int three()
-{
-  return 3; /* This function returns 3 so essentially 3 is stored in the function
-            */
-}
-
 int main() {
 
   std::cout << "Please enter a number" << '\n';
