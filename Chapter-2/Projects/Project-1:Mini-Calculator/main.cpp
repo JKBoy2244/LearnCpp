b5875ef332b61024b8b45b044e81ae186bc54d95
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 00:24:45 +0100

    Update main.cpp

--- Chapter-2/Projects/Project-1:Mini-Calculator/main.cpp
+++ Chapter-2/Projects/Project-1:Mini-Calculator/main.cpp
@@ -1,2 +1,54 @@
 #include <iostream>
 
+int firstInteger() {
+
+  std::cout << "Enter a first integer"; //Prompts the user to enter first integer
+  int integerOne {};
+  std:: cin >> integerOne;
+  return integerOne;
+}
+
+int secondInteger() {
+
+  std::cout << "Enter a second integer"; //Prompts the user to enter second integer
+  int integerTwo {};
+  std:: cin >> integerTwo;
+  return integerTwo;
+}
+
+char operatorSelect() {
+
+  std::cout << "Choose an operator sign from either: +, -, /, * "; //Prompts the user to enter an operator character
+  char sign{};
+  std::cin >> sign;
+  return sign;
+}
+
+void calculate(int valueOne, int valueTwo, char operatorSign) {
+
+  if (operatorSign == '+') {
+    std::cout << valueOne << " + " << valueTwo << " = " << valueOne + valueTwo;  //Calculates the result based on the chosen operation
+  }
+
+  if (operatorSign == '-') {
+    std::cout << valueOne << " - " << valueTwo << " = " << valueOne - valueTwo;
+  }
+
+  if (operatorSign == '/') {
+    std::cout << valueOne << " / " << valueTwo << " = " << valueOne / valueTwo;
+  }
+
+  if (operatorSign == '*') {
+    std::cout << valueOne << " * " << valueTwo << " = " << valueOne * valueTwo;
+  }
+  
+}
+
+int main() {
+
+  int valueOne = firstInteger(); 
+  char operatorSign = operatorSelect();       //The compiler runs this method in sequential order
+  int valueTwo = secondInteger();
+  calculate(valueOne, valueTwo, operatorSign);
+  
+}
