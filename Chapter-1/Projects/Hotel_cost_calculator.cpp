778a0c1a957d4953c1e21bf77c9a977e8018cdfa
JKBoy2244 <jobayer2006@gmail.com>
2026-07-27 16:40:56 +0100

    Update Hotel_cost_calculator.cpp

--- Chapter-1/Projects/Hotel_cost_calculator.cpp
+++ Chapter-1/Projects/Hotel_cost_calculator.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+int main() 
+{
+
+  [[maybe_unused]] double childrenCost {12.99}; //Cost per child when booking a hotel 
+  [[maybe_unused]] double teenagerCost {21.99}; //Cost per teenager when booking a hotel 
+  [[maybe_unused]] double adultCost {32.99}; //Cost per adult when booking a hotel 
+  [[maybe_unused]] int nightCost {30}; //Cost per night to stay
+  int extraCharge = {50};
+
+  std::cout << "How many children are occupying the hotel room?"; //Program prompts how many children are staying in the hotel room being booked
+  int children{};
+  std::cin >> children;
+
+  std::cout << "How many teenagers are occupying the hotel room?"; //Program prompts how many teenagers are staying in the hotel room being booked
+  int teenagers{};
+  std::cin >> teenagers;
+
+  std::cout << "How many adults are occupying the hotel room?"; //Program prompts how many adults are staying in the hotel room being booked
+  int adults{};
+  std::cin >> adults;
+
+  std::cout << "How many nights are you staying for?"; //Program prompts how many nights they're staying in the hotel room being booked
+  int nights{};
+  std::cin >> nights;
+
+  double totalCharge { (childrenCost*children) + (teenagerCost*teenagers) + (adultCost*adults) + (nightCost*nights) + extraCharge }; //Calculates the total booking charge to be paid in order to get access
+  std::cout << "The total cost of this booking is £" << totalCharge << '\n';
+  
+}
