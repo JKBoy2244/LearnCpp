2f5673db7f146c5479d41691256e6fdf9e555a62
JKBoy2244 <jobayer2006@gmail.com>
2026-07-27 17:12:29 +0100

    Update multiply-by-9-calculator.cpp

--- Chapter-1/Projects/multiply-by-9-calculator.cpp
+++ Chapter-1/Projects/multiply-by-9-calculator.cpp
@@ -20,5 +20,7 @@ int main() {
   int result { nine() * number };       //Result gets a new value when multiplying the inputted number by 9
   std::cout << number << " multiplied by 9 = " << result << '\n';                 //Displays the result on the screen
   std::cout << number << " multiplied by 3 = " << three() * number <<'\n';       //Displays another answer by multiplying the same inputted number by 3
+
+  return 0;
   
 }
