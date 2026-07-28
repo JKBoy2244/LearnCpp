d6d855b86fede100500d041322798dbe1d25eadd
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 15:53:41 +0100

    Update main.cpp

--- Chapter-2/Projects/Project-2:NumberOfBricksCalculation/main.cpp
+++ Chapter-2/Projects/Project-2:NumberOfBricksCalculation/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-double cuboidHouseVolume() {
+double HouseVolume() {
 
   cout << "Enter the height of the cuboid portion of a house in metres?\n";  //Height is one measurement tool needed to calculate the cuboid portion of the house (along with width and length)
   double cuboidHeight {};
@@ -18,44 +18,38 @@ double cuboidHouseVolume() {
   double cuboidVolume {cuboidHeight * cuboidWidth * cuboidLength};
   cout << "The volume of the cuboid portion of the house is " << cuboidVolume << " metres cubed\n";   //Find the total volume of the cuboid portion of the house
 
-  return cuboidVolume;
-}
-
-double triangularPrismHouseVolume(double cuboidLength, double cuboidWidth) {
-
+  //As width and length values are stored, this is important for calculating the volume of the triangular portion of the house
   cout << "Now for the triangular prism portion of the house enter the height of the prism portion of the house in metres (length and width already obtained)?\n";
-  double prismHeight {};
-  cin >> prismHeight;                                                                                          //As width and length values are stored, this is important for calculating the volume of the triangular portion of the house
+  double prismHeight {};          
+  cin >> prismHeight;
 
   double prismVolume {0.5 * cuboidLength * cuboidWidth * prismHeight};
   cout << "The volume of the prism portion of the house is " << prismVolume << " metres cubed\n";           //Find the total volume of the triangular prism portion of the house
 
-  return prismVolume;
-}
-
-double totalVolume(double cuboidVolume, double prismVolume) {
-
   double houseVolume {cuboidVolume + prismVolume};
   cout << "The volume of the house is " << houseVolume << " metres cubed\n";                         //Combines the 2 returned values passed as parameters to find the final volume of the whole house
   return houseVolume;
+  
 }
 
-double oneBrickVolume() {
+double oneBrickVolume() {                                                                                     /*Similar process for both separate portions of the house however here, it's only a volume of a brick.
+                                                                                                              This is very important because it's used to calculate how many whole bricks are needed to build a house
+                                                                                                              */
+  
+  cout << "Enter the height of a small brick used to build the house in metres?\n";                                     
+  double brickHeight {};                                                                                        
+  cin >> brickHeight;                                                                                         
 
-  cout << "Enter the height of a small brick used to build the house?\n";                                     /*Similar process for both separate portions of the house however here, it's only a volume of a brick.
-  double brickHeight {};                                                                                        This is very important because it's used to calculate how many whole bricks are needed to build a house
-  cin >> brickHeight;                                                                                         */
-
-  cout << "Enter the width of a small brick used to build the house?\n";
+  cout << "Enter the width of a small brick used to build the house in metres?\n";
   double brickWidth {};
   cin >> brickWidth;
 
-  cout << "Enter the length of a small brick used to build the house?\n";
+  cout << "Enter the length of a small brick used to build the house in metres?\n";
   double brickLength {};
   cin >> brickLength;
 
   double brickVolume {brickHeight * brickWidth * brickLength};
-  cout << "The volume of the cuboid portion of the house is " << brickVolume << " metres cubed\n";
+  cout << "The volume of the one brick of the house is " << brickVolume << " metres cubed\n";
 
   return brickVolume;
   
@@ -75,9 +69,9 @@ void workRate(int numberOfBricks) {
   cin >> numberOfWorkers;
 
   cout << " ---------------------------------------------------------------------------------------------------------------------------------- \n";
-  double days {numberOfBricks / numberOfWorkers};
+  double days = static_cast<double>(numberOfBricks) / numberOfWorkers;                   //This is slightly outside the syllabus but only this (rest is in learncpp ch. 2 syllabus but it's to prevent calculation error
   double hours {days * 24};
-  double minutes {hours * 60};
+  double minutes {hours * 60};                                                          //Conversion rates
   double seconds {minutes * 60};
   double weeks {days / 7};
   double months {weeks / 4.33};
@@ -87,7 +81,7 @@ void workRate(int numberOfBricks) {
   cout << "The total number of seconds this will take to build a full house from scratch is: " << seconds << " seconds\n";                //This block part tells you the time taken to complete the work in various formats
   cout << "The total number of minutes this will take to build a full house from scratch is: " << minutes << " minutes\n";
   cout << "The total number of hours this will take to build a full house from scratch is: " << hours << " hours\n";
-  cout << "The total number of days this will take to build a full house from scratch is: " << days << " days\n";
+  cout << "The total number of full days this will take to build a full house from scratch is: " << days << " days\n";
   cout << "The total number of weeks this will take to build a full house from scratch is: " << weeks << " weeks\n";
   cout << "The total number of months this will take to build a full house from scratch is: " << months << " months\n";
   cout << "The total number of years this will take to build a full house from scratch is: " << years << " years\n";
@@ -97,10 +91,8 @@ void workRate(int numberOfBricks) {
 }
 
 int main() {
-
-  double cuboidVolume = cuboidHouseVolume();
-  double prismVolume = triangularPrismHouseVolume(cuboidLength, cuboidWidth);                       //Compiler executes this main function in sequential order
-  double houseVolume = totalVolume(cuboidVolume, prismVolume);
+  
+  double houseVolume = HouseVolume();                                             //Compiler executes this main function in sequential order
   double brickVolume = oneBrickVolume();
   int numberOfBricks = totalNumberOfBricks(houseVolume, brickVolume);
   workRate(numberOfBricks);
