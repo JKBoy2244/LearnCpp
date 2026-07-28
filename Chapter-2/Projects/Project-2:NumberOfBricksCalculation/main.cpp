531074c66ad7138c73b23beef8c96dc305c14ac0
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 15:33:55 +0100

    Update main.cpp

--- Chapter-2/Projects/Project-2:NumberOfBricksCalculation/main.cpp
+++ Chapter-2/Projects/Project-2:NumberOfBricksCalculation/main.cpp
@@ -63,8 +63,8 @@ double oneBrickVolume() {
 
 int totalNumberOfBricks(double houseVolume, double brickVolume) {
 
-  int totalNumberOfBricks (houseVolume / brickVolume); 
-  cout << "The total Number of whole bricks you need is " << totalNumberOfBricks << " bricks.\n";                                     //Total number of whole bricks required to build a full house
+  int numberOfBricks (houseVolume / brickVolume); 
+  cout << "The total Number of whole bricks you need is " << numberOfBricks << " bricks.\n";                                     //Total number of whole bricks required to build a full house
   return numberOfBricks;
 }
 
