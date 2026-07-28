df32287fb578e6f233112c7fe19fd607e767cb1c
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 12:53:34 +0100

    Update operatorSelect.cpp

--- Chapter-2/Projects/Project-1:Mini-Calculator/operatorSelect.cpp
+++ Chapter-2/Projects/Project-1:Mini-Calculator/operatorSelect.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
 
+char operatorSelect() {
+
+  cout << "Choose an operator sign from either: +, -, /, * "; //Prompts the user to enter an operator character
+  char sign{};
+  cin >> sign;
+  return sign;
+}
