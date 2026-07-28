9bde66125322bbc4c0f82846faa7751f651c6b16
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 12:52:43 +0100

    Update firstInteger.cpp

--- Chapter-2/Projects/Project-1:Mini-Calculator/firstInteger.cpp
+++ Chapter-2/Projects/Project-1:Mini-Calculator/firstInteger.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
 
+int firstInteger() {
+
+  cout << "Enter a first integer"; //Prompts the user to enter first integer
+  int integerOne {};
+  cin >> integerOne;
+  return integerOne;
+}
