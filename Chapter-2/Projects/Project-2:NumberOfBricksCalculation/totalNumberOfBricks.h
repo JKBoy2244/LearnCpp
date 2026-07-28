c5c02d4ae6be6bf02436a92f770ed2579c868c07
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 15:57:12 +0100

    Create totalNumberOfBricks.h

---
+++ Chapter-2/Projects/Project-2:NumberOfBricksCalculation/totalNumberOfBricks.h
@@ -0,0 +1,9 @@
+#ifndef TOTALNUMBEROFBRICKS
+#define TOTALNUMBEROFBRICKS
+
+int totalNumberOfBricks(double houseVolume, double brickVolume) {
+
+  int numberOfBricks (houseVolume / brickVolume); 
+  cout << "The total Number of whole bricks you need is " << numberOfBricks << " bricks.\n";                                     //Total number of whole bricks required to build a full house
+  return numberOfBricks;
+}
