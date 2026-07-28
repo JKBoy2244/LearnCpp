f0897118297bb346fcbf2c346adf5e98c3a53768
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 12:55:44 +0100

    Refactor Mini-Calculator code structure
    Removed unnecessary preprocessor directives and function implementations for a cleaner code structure. Updated function signatures for better clarity.

--- Chapter-2/Projects/Project-1:Mini-Calculator/main.cpp
+++ Chapter-2/Projects/Project-1:Mini-Calculator/main.cpp
@@ -1,64 +1,10 @@
 #include <iostream>
 using namespace std;
 
-#define add
-#define subtract
-#define multiply
-#define divide
-
-int firstInteger() {
-
-  cout << "Enter a first integer"; //Prompts the user to enter first integer
-  int integerOne {};
-  cin >> integerOne;
-  return integerOne;
-}
-
-int secondInteger() {
-
-  cout << "Enter a second integer"; //Prompts the user to enter second integer
-  int integerTwo {};
-  cin >> integerTwo;
-  return integerTwo;
-}
-
-char operatorSelect() {
-
-  cout << "Choose an operator sign from either: +, -, /, * "; //Prompts the user to enter an operator character
-  char sign{};
-  cin >> sign;
-  return sign;
-}
-
-void calculate(int valueOne, int valueTwo, char operatorSign) {
-
-    switch (operatorSign) {
-
-#ifdef add
-      case '+':       //Case statements (plus breaks) are outside of the learncpp chapter1+chapter2 scope but it had to be used becuase if statements aren't allowed to be used in this project 
-        cout << valueOne << " + " << valueTwo << " = " << valueOne + valueTwo;  //Calculates the result based on the chosen operation (addition)
-        break;
-#endif
-
-#ifdef subtract
-      case '-': 
-        cout << valueOne << " - " << valueTwo << " = " << valueOne - valueTwo;   //Calculates the result based on the chosen operation (subtraction)
-        break;
-#endif
-
- #ifdef divide
-      case '/':
-        cout << valueOne << " / " << valueTwo << " = " << valueOne / valueTwo;   //Calculates the result based on the chosen operation (division)
-        break; 
-#endif
-
-#ifdef multiply
-      case '*':
-        cout << valueOne << " * " << valueTwo << " = " << valueOne * valueTwo;   //Calculates the result based on the chosen operation (multiplication)
-        break;
-#endif
-    }
-}
+int firstInteger();
+int secondInteger();
+char operatorSelect();
+void calculate(valueOne, valueTwo, operatorSign);
 
 int main() {
 
