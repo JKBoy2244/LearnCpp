038dcaf07c4724f752e07d17d4732c1aed22c20f
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 12:49:32 +0100

    Update main.cpp

--- Chapter-2/Projects/Project-1:Mini-Calculator/main.cpp
+++ Chapter-2/Projects/Project-1:Mini-Calculator/main.cpp
@@ -1,47 +1,63 @@
 #include <iostream>
+using namespace std;
+
+#define add
+//#define subtract
+//#define multiply
+//#define divide
 
 int firstInteger() {
 
-  std::cout << "Enter a first integer"; //Prompts the user to enter first integer
+  cout << "Enter a first integer"; //Prompts the user to enter first integer
   int integerOne {};
-  std:: cin >> integerOne;
+  cin >> integerOne;
   return integerOne;
 }
 
 int secondInteger() {
 
-  std::cout << "Enter a second integer"; //Prompts the user to enter second integer
+  cout << "Enter a second integer"; //Prompts the user to enter second integer
   int integerTwo {};
-  std:: cin >> integerTwo;
+  cin >> integerTwo;
   return integerTwo;
 }
 
 char operatorSelect() {
 
-  std::cout << "Choose an operator sign from either: +, -, /, * "; //Prompts the user to enter an operator character
+  cout << "Choose an operator sign from either: +, -, /, * "; //Prompts the user to enter an operator character
   char sign{};
-  std::cin >> sign;
+  cin >> sign;
   return sign;
 }
 
 void calculate(int valueOne, int valueTwo, char operatorSign) {
 
-  if (operatorSign == '+') {
-    std::cout << valueOne << " + " << valueTwo << " = " << valueOne + valueTwo;  //Calculates the result based on the chosen operation
-  }
+    switch (operatorSign) {
 
-  if (operatorSign == '-') {
-    std::cout << valueOne << " - " << valueTwo << " = " << valueOne - valueTwo;
-  }
+#ifdef add
+      case '+':       //Case statements (plus breaks) are outside of the learncpp chapter1+chapter2 scope but it had to be used becuase if statements aren't allowed to be used in this project 
+        cout << valueOne << " + " << valueTwo << " = " << valueOne + valueTwo;  //Calculates the result based on the chosen operation (addition)
+        break;
+#endif
 
-  if (operatorSign == '/') {
-    std::cout << valueOne << " / " << valueTwo << " = " << valueOne / valueTwo;
-  }
+#ifdef subtract
+      case '-': 
+        cout << valueOne << " - " << valueTwo << " = " << valueOne - valueTwo;   //Calculates the result based on the chosen operation (subtraction)
+        break;
+#endif
 
-  if (operatorSign == '*') {
-    std::cout << valueOne << " * " << valueTwo << " = " << valueOne * valueTwo;
-  }
-  
+ #ifdef divide
+      case '/':
+        cout << valueOne << " / " << valueTwo << " = " << valueOne / valueTwo;   //Calculates the result based on the chosen operation (division)
+        break; 
+#endif
+
+#ifdef multiply
+      case '*':
+        cout << valueOne << " * " << valueTwo << " = " << valueOne * valueTwo;   //Calculates the result based on the chosen operation (multiplication)
+        break;
+#endif
+    }
 }
 
 int main() {
