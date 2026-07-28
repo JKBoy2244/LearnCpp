646dee275774c7d22644c6365a380eea47671862
JKBoy2244 <jobayer2006@gmail.com>
2026-07-28 12:53:08 +0100

    Update secondInteger.cpp

--- Chapter-2/Projects/Project-1:Mini-Calculator/secondInteger.cpp
+++ Chapter-2/Projects/Project-1:Mini-Calculator/secondInteger.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+using namespace std;
 
+int secondInteger() {
+
+  cout << "Enter a second integer"; //Prompts the user to enter second integer
+  int integerTwo {};
+  cin >> integerTwo;
+  return integerTwo;
+}
