9f16f8876cc736bc42c41e20c6822bd05eb53407
JKBoy2244 <jobayer2006@gmail.com>
2026-07-30 12:16:51 +0100

    Update main.cpp

--- Chapter-2/Projects/Project-3:University-Grade-Calculator/main.cpp
+++ Chapter-2/Projects/Project-3:University-Grade-Calculator/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+double firstYear();
+double firstYearWeighted(double grade);                                                //All methods forward declaration
+double secondYear();
+double secondYearWeighted(double grade);
+double thirdYear();
+double thirdYearWeighted(double grade);
+void final(double first, double second, double third);
+
+
 int main() {
 
   double firstYearGrade = firstYear();
