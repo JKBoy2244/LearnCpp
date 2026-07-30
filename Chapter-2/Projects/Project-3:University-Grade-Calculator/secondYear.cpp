96043aa57ab5b33d81cbed13dcbf987c89e265c1
JKBoy2244 <jobayer2006@gmail.com>
2026-07-30 14:44:22 +0100

    Update and rename secondYearWeighted.cpp to secondYear.cpp

--- Chapter-2/Projects/Project-3:University-Grade-Calculator/secondYear.cpp
+++ Chapter-2/Projects/Project-3:University-Grade-Calculator/secondYear.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #include "secondYear.h"
 
-double secondYearWeighted(double secondYearGrade) {
+double secondYear(double secondYearGrade) {
 
   double secondYearWeightedGrade {0.3 * secondYearGrade};
   cout << "Your second year weighted grade is " << secondYearWeightedGrade << "%\n";         //Second year normally counts for 10% of the final grade
