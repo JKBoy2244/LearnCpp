8a2f5393b3f2a655a7fe1a8842dbf83e56d4683b
JKBoy2244 <jobayer2006@gmail.com>
2026-07-30 14:44:08 +0100

    Rename function from firstYearWeighted to firstYear

--- Chapter-2/Projects/Project-3:University-Grade-Calculator/firstYear.cpp
+++ Chapter-2/Projects/Project-3:University-Grade-Calculator/firstYear.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #include "firstYear.h"
 
-double firstYearWeighted(double firstYearGrade) {
+double firstYear(double firstYearGrade) {
 
   double firstYearWeightedGrade {0.1 * firstYearGrade};
   cout << "Your first year weighted grade is " << firstYearWeightedGrade << "%\n";     //First year normally counts for 10% of the final grade
