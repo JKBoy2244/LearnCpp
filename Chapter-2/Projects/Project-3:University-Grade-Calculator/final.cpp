8bcc7ef5302ea0d9fb129e6aac95bd0f9aa025b8
JKBoy2244 <jobayer2006@gmail.com>
2026-07-30 12:12:53 +0100

    Create final.cpp

---
+++ Chapter-2/Projects/Project-3:University-Grade-Calculator/final.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+void final(double firstYearWeightedGrade, double secondYearWeightedGrade, double thirdYearWeightedGrade) {
+
+  double finalUniversityGrade {firstYearWeightedGrade + secondYearWeightedGrade + thirdYearWeightedGrade};                      //Calculates the overall final score as a % using the combined weighted scores
+  cout << "Your final university grade is " << finalUniversityGrade << "%\n";
+}
