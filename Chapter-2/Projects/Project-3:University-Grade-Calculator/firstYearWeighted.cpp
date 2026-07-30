d5f9834d8b1ac374500eed6ad38db3c93406d830
JKBoy2244 <jobayer2006@gmail.com>
2026-07-30 12:08:27 +0100

    Add firstYearWeighted function for grade calculation

---
+++ Chapter-2/Projects/Project-3:University-Grade-Calculator/firstYearWeighted.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "firstYear.h"
+
+double firstYearWeighted(double firstYearGrade) {
+
+  double firstYearWeightedGrade {0.1 * firstYearGrade};
+  cout << "Your first year weighted grade is " << firstYearWeightedGrade << "%\n";     //First year normally counts for 10% of the final grade
+  return firstYearWeightedGrade;  
+}
