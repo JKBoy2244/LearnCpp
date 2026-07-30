204abbbadf10e82f8dbd0b03b27648ce1c624021
JKBoy2244 <jobayer2006@gmail.com>
2026-07-30 14:44:42 +0100

    Update and rename thirdYearWeighted.cpp to thirdYear.cpp

--- Chapter-2/Projects/Project-3:University-Grade-Calculator/thirdYear.cpp
+++ Chapter-2/Projects/Project-3:University-Grade-Calculator/thirdYear.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #include "thirdYear.h"
 
-double thirdYearWeighted(double thirdYearGrade) {
+double thirdYear(double thirdYearGrade) {
 
   double thirdYearWeightedGrade {0.6 * thirdYearGrade};
   cout << "Your third year weighted grade is " << thirdYearWeightedGrade << "%\n";                         //Third year normally counts for 10% of the final grade
