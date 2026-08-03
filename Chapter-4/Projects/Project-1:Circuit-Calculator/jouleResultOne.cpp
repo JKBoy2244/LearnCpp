1df1b7eab26ee63aedf19e6023e62e2742de6132
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 16:56:17 +0100

    Update jouleResultOne.cpp

--- Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultOne.cpp
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultOne.cpp
@@ -9,10 +9,10 @@ void jouleResultOne(int energyOption_one) {
   double power {};
   cin >> power;
 
-  cout << "Enter the time of the circuit\n";
+  cout << "Enter the time of the circuit in milliseconds in integer form\n";
   unsigned int time {};
   cin >> time;
 
-  double energy {power * static_cast<double>time};
+  double energy {power * (static_cast<double>(time) / 1000)};
   cout << "Energy result of the circuit is " << energy << " joules\n";
 }
