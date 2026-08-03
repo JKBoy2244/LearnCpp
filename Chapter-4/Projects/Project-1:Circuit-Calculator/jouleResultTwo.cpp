b43a84237d6ebf1c6dc84e73251b0cad9eab1a12
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:42:00 +0100

    Add jouleResultTwo function and include header

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultTwo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#include "jouleResultTwo.h";
+
+void jouleResultTwo(int energyOption_two) {
+
+  cout << "Enter the voltage of the circuit\n";
+  double voltage {};
+  cin >> voltage;
+
+  cout << "Enter the current of the circuit\n";
+  double current {};
+  cin >> current;
+
+  cout << "Enter the time of the circuit\n";
+  unsigned double time {};
+  cin >> time;
+
+  double energy {voltage * current * time};
+  cout << "Energy result of the circuit is " << energy << " volts\n";
+}
