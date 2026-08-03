a375209723c6079cec73e6201120cc046ce6f213
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:43:50 +0100

    Create jouleResultThree.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultThree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "jouleResultThree.h";
+
+void jouleResultThree(int energyOption_three) {
+
+  cout << "Enter the voltage of the circuit\n";
+  double voltage {};
+  cin >> voltage;
+
+  cout << "Enter the charge of the circuit\n";
+  double charge {};
+  cin >> charge;
+
+  double energy {charge * voltage};
+  cout << "Energy result of the circuit is " << energy << " volts\n";
+}
