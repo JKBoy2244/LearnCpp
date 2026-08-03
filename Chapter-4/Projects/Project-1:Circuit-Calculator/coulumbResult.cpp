ffac9c1d53b25b12b024a7b907f6ce7387972a7e
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 12:59:40 +0100

    Create coulumbResult.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/coulumbResult.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "coulumbResult.h";
+
+void coulumbResult(int chargeOption_one) {
+
+  cout << "Enter the current of the circuit\n";
+  double current {};
+  cin >> current;
+
+  cout << "Enter the time of the circuit\n";
+  unsigned double time {};
+  cin >> time;
+
+  double charge {current * time};
+  cout << "Charge result of the circuit is " << charge << " volts\n";
+}
