9cfa4e700246fda9e31057be0bb960ec4101d309
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 16:54:27 +0100

    Refine time input and energy calculation in jouleResultFive
    Updated time input prompt and adjusted energy calculation to convert time from milliseconds to seconds.

--- Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultFive.cpp
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultFive.cpp
@@ -13,10 +13,10 @@ void jouleResultFive(int energyOption_five) {
   double resistance {};
   cin >> resistance;
 
-  cout << "Enter the time of the circuit\n";
+  cout << "Enter the time of the circuit in milliseconds in integer form\n";
   unsigned int time {};
   cin >> time;
 
-  double energy {(voltage * voltage * static_cast<double>time) / resistance};
+  double energy {(voltage * voltage * (static_cast<double>(time) *1000)) / resistance};
   cout << "Energy result of the circuit is " << energy << " joules\n";
 }
