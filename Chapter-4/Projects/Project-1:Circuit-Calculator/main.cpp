d75bce85f66e9e1d063c4f4742173e7b08f5e8a3
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 12:49:02 +0100

    Update main.cpp

--- Chapter-4/Projects/Project-1:Circuit-Calculator/main.cpp
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/main.cpp
@@ -52,3 +52,90 @@ int volts(char voltage) {
     return voltageOption_one;
   }  
 }
+
+int coulumb(char charge) {
+
+  cout << "Since you choose charge, choose the option below to continue as 1 (option 1). below is the only option\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+  cout << "1). Q = It\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+
+  int chargeOption {};
+  cin >> chargeOption;
+
+  if (chargeOption == 1) {
+
+    int chargeOption_one = chargeOption;
+    return chargeOption_one;
+  }  
+}
+
+int watts(char power) {
+
+  cout << "Since you choose power, choose either option 1, 2, or 3 below to continue\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+  cout << "1). P = VI, 2). P = (I*I)*R, 3). P = (V*V)/R\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+
+  int powerOption {};
+  cin >> powerOption;
+
+  if (powerOption == 1) {
+
+    int powerOption_one = powerOption;
+    return powerOption_one;
+  }  
+
+  if (powerOption == 2) {
+
+    int powerOption_two = powerOption;
+    return powerOption_two;
+  }  
+
+  if (powerOption == 3) {
+
+    int powerOption_three = powerOption;
+    return powerOption_three;
+  }  
+}
+
+int joules(char energy) {
+
+  cout << "Since you choose energy, choose either option 1, 2, 3, 4 or 5 below to continue\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+  cout << "1). P = VI, 2). P = (I*I)*R, 3). P = (V*V)/R\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+
+  int energyOption {};
+  cin >> energyOption;
+
+  if (energyOption == 1) {
+
+    int energyOption_one = energyOption;
+    return energyOption_one;
+  }  
+
+  if (energyOption == 2) {
+
+    int energyOption_two = energyOption;
+    return energyOption_two;
+  } 
+
+  if (energyOption == 3) {
+
+    int energyOption_three = energyOption;
+    return energyOption_three;
+  }
+
+  if (energyOption == 4) {
+
+    int energyOption_four = energyOption;
+    return energyOption_four;
+  }
+
+  if (energyOption == 5) {
+
+    int energyOption_five = energyOption;
+    return energyOption_five;
+  }
+}
