6e65145805b9f2b1a402733248a5c182dce71b90
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:46:44 +0100

    Create jouleResultFive.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultFive.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#include "jouleResultFive.h";
+
+void jouleResultFive(int energyOption_five) {
+
+  cout << "Enter the voltage of the circuit\n";
+  double voltage {};
+  cin >> voltage;
+
+  cout << "Enter the resistance of the circuit\n";
+  double resistance {};
+  cin >> resistance;
+
+  cout << "Enter the time of the circuit\n";
+  unsigned double time {};
+  cin >> time;
+
+  double energy {(voltage * voltage * time) / resistance};
+  cout << "Energy result of the circuit is " << energy << " volts\n";
+}
