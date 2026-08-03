a161185af92da2e3cfe9ab36f955d55b344e7ee8
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:40:31 +0100

    Add include directive for iostream in jouleResultOne.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultOne.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "jouleResultOne.h";
+
+void jouleResultOne(int energyOption_one) {
+
+  cout << "Enter the power of the circuit\n";
+  double power {};
+  cin >> power;
+
+  cout << "Enter the time of the circuit\n";
+  unsigned double time {};
+  cin >> time;
+
+  double energy {power * time};
+  cout << "Energy result of the circuit is " << energy << " volts\n";
+}
