6e266b8f6b2b61ebbe41027ef151fa854920c366
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 12:11:24 +0100

    Update and rename unitChoose.cpp to optionCalculate.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/optionCalculate.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+#include #optionCalculate.h";
+
+char optionCalculate() {
+
+  cout << "Choose which 4 specific values you want to calculate here (the rest of the values you have to measure internally by yourself?\n";
+  cout << "-------------------------------------------------------------------------------------------------------------------------------"\n";
+  cout << "V = voltage, P = power, C = charge, E = energy";
+  cout << "-------------------------------------------------------------------------------------------------------------------------------"\n";
+
+  char optionLetter {};
+  cin >> optionLetter;
+
+  if (optionLetter == 'V') {
+
+    voltage = optionLetter;
+    return voltage;
+  }
+
+  if (optionLetter == 'P') {
+
+    power = optionLetter;
+    return power;
+  }
+  
+  if (optionLetter == 'C') {
+
+    charge = optionLetter;
+    return charge;
+  }
+  
+  if (optionLetter == 'E') {
+
+    energy = optionLetter;
+    return energy;
+  }
+}
+
