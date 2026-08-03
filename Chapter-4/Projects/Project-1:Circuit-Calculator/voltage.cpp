4cd8b0b3d1024112410ccf120f59ca444866927f
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 12:18:20 +0100

    Add voltage function with user option handling
    Implement voltage calculation option for user input.

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/voltage.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#include "voltage.h";
+
+int voltage(char voltage) {
+
+  cout << "Since you choose voltage, choose the option below to continue as 1 (option 1). below is the only option\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+  cout << "1). V = IR\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+
+  int voltageOption {};
+  cin >> voltageOption;
+
+  if (voltageOption == 1) {
+
+    int voltageOption_one = voltageOption;
+    return voltageOption_one;
+  }  
+}
