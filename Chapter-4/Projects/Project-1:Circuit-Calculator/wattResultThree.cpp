4c5d6cb795ad2ddaa5d3cb87ed7509cca318cf17
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:29:53 +0100

    Add wattResultThree function for power calculation

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/wattResultThree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "wattResultThree.h";
+
+void wattResultThree(int powerOption_three) {
+
+  cout << "Enter the voltage of the circuit\n";
+  double voltage {};
+  cin >> voltage;
+
+  cout << "Enter the resistance of the circuit\n";
+  double resistance {};
+  cin >> resistance;
+
+  double power {(voltage * voltage) / resistance};
+  cout << "Power result of the circuit is " << power << " volts\n";
+}
