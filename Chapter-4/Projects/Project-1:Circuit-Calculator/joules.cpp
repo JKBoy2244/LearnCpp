5c7f7b32c106f372fa56e8840155df6b77026052
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 12:48:11 +0100

    Update joules.cpp

--- Chapter-4/Projects/Project-1:Circuit-Calculator/joules.cpp
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/joules.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+#include "joules.h";
+
+int joules(char energy) {
+
+  cout << "Since you choose energy, choose either option 1, 2, 3, 4 or 5 below to continue\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+  cout << "1). P = VI, 2). P = (I*I)*R, 3). P = (V*V)/R\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+
+  int energyOption {};
+  cin >> energyOption;
+
+  if (energyOption == 1) {
+
+    int energyOption_one = energyOption;
+    return energyOption_one;
+  }  
+
+  if (energyOption == 2) {
+
+    int energyOption_two = energyOption;
+    return energyOption_two;
+  } 
+
+  if (energyOption == 3) {
+
+    int energyOption_three = energyOption;
+    return energyOption_three;
+  }
+
+  if (energyOption == 4) {
+
+    int energyOption_four = energyOption;
+    return energyOption_four;
+  }
+
+  if (energyOption == 5) {
+
+    int energyOption_five = energyOption;
+    return energyOption_five;
+  }
+}
