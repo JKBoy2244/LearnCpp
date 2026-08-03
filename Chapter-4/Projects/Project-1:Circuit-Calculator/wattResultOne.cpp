fdf112fd1be8199499d9ac0a43f04831a433c05b
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:27:57 +0100

    Create wattResultOne.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/wattResultOne.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "wattResultOne.h";
+
+void wattResultOne(int powerOption_one) {
+
+  cout << "Enter the voltage of the circuit\n";
+  double voltage {};
+  cin >> voltage;
+
+  cout << "Enter the current of the circuit\n";
+  double current {};
+  cin >> current;
+
+  double power {current * voltage};
+  cout << "Power result of the circuit is " << power << " volts\n";
+}
