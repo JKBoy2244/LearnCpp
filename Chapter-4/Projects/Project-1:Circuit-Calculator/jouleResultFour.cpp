4b4bc3558842a25f3573dedd66bb0de5c4abcb08
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 13:45:16 +0100

    Create jouleResultFour.cpp

---
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/jouleResultFour.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#include "jouleResultFour.h";
+
+void jouleResultFour(int energyOption_four) {
+
+  cout << "Enter the current of the circuit\n";
+  double current {};
+  cin >> current;
+
+  cout << "Enter the resistance of the circuit\n";
+  double resistance {};
+  cin >> resistance;
+
+  cout << "Enter the time of the circuit\n";
+  unsigned double time {};
+  cin >> time;
+
+  double energy {current * current * resistance * time};
+  cout << "Energy result of the circuit is " << energy << " volts\n";
+}
