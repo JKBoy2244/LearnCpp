ba378fc8fc6bd5e1b4c5dbf993c14f11f3b4fe1b
JKBoy2244 <jobayer2006@gmail.com>
2026-08-03 12:41:21 +0100

    Update coulumb.cpp

--- Chapter-4/Projects/Project-1:Circuit-Calculator/coulumb.cpp
+++ Chapter-4/Projects/Project-1:Circuit-Calculator/coulumb.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
 
+#include "coulumb.h"
+
+int coulumb(char charge) {
+
+  cout << "Since you choose charge, choose the option below to continue as 1 (option 1). below is the only option\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+  cout << "1). Q = It\n";
+  cout << "---------------------------------------------------------------------------------------------------------";
+
+  int chargeOption {};
+  cin >> chargeOption;
+
+  if (chargeOption == 1) {
+
+    int chargeOption_one = voltageOption;
+    return chargeOption_one;
+  }  
+}
