0de82c3c46011f46b5e15ea7449799c20568502a
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 23:24:27 +0100

    Rename variable for first name input

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
@@ -9,6 +9,6 @@ string firstName() {
   cout << "Tell me your first name?\n";      //Prompts user to type first name?
   string first {};
   cin >> first;
-  string_view firstName_input = first;
-  return firstName_input;
+  string_view firstNameInput = first;
+  return firstNameInput;
 }
