5e39059390d642613aa95afa2c92903d1a269d9e
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 22:46:28 +0100

    Create classification.cpp

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/classification.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "classification.h";
+
+string classification(double finalMark) {
+
+   if (finalMark >= 70.0) {
+
+      string finalGrade = "First Class";
+      return finalGrade;
+   }
+
+   else if (finalMark >= 60.0 and finalMark < 70.0) {
+
+     string finalGrade = "Upper Second Class Honours";
+     return finalGrade;
+   }
+
+   else if (finalMark >= 50.0 and finalMark < 50.0) {
+
+     string finalGrade = "Lower Second Class Honours";
+     return finalGrade;
+   }
+
+  else if (finalMark >= 40 and finalMark < 50.0) {
+
+     string finalGrade = "Third Class Honours";
+     return finalGrade;
+   }
+
+  else {
+ 
+     string finalGrade = "Fail";
+     return finalGrade;
+  }
