21956b05007403bb8e560accddfa808bf9bde2c0
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 00:00:45 +0100

    Update firstName.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
@@ -9,6 +9,6 @@ string firstName() {
   cout << "Tell me your first name?\n";      //Prompts user to type first name?
   string first {};
   cin >> first;
-  string_view firstNameInput = first;
+  //string_view firstNameInput = first;
   return first;
 }
