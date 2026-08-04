56a7634915622f9e520698fc38f6720aa5890860
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 21:55:12 +0100

    Create firstName.cpp

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>;
+using namespace std;
+
+#include "firstName.h";
+
+string firstName() {
+
+  cout << "Tell me your first name?\n";            //Prompts user to type first name?
+  string first {};
+  cin >> first;
+  return first;
+}
