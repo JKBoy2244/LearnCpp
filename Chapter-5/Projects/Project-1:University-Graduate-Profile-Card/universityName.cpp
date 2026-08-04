1b392b1047723d3d313a9ff3d64e01193f75e7eb
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 00:01:07 +0100

    Update universityName.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/universityName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/universityName.cpp
@@ -9,6 +9,6 @@ string universityName() {
   cout << "Which University did you attend?\n";            //Prompts user to type university name?
   string university {};
   cin >> university;
-  string_view universityNameInput = university;
+  //string_view universityNameInput = university;
   return university;
 }
