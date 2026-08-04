9d6cea6362be2272512622816599a557a4b3a4e5
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 23:29:10 +0100

    Update fullProfileCard.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 #include "fullProfileCard.h"
 
-void fullProfileCard(string_view firstNameInput, string_view lastNameInput, string_view universityNameInput, string_view courseNameInput, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
+void fullProfileCard(string first, string last, string university, string course, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
 double secondYearWeightingResult, double thirdYearWeightingResult, double finalMark, string finalGrade, int internshipNumber) {
 
    cout << "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
    cout << "UNIVERSITY STUDENT GRADUATE PROFILE CARD!\n";
    cout << "-------------------------------------------";
-   cout << "First name : " << firstNameInput << endl;
-   cout << "Last name : " << lastNameInput << endl;
-   cout << "University name : " << universityNameInput << endl;
-   cout << "Course name : " << courseNameInput << endl;
+   cout << "First name : " << first << endl;
+   cout << "Last name : " << last << endl;
+   cout << "University name : " << university << endl;
+   cout << "Course name : " << course << endl;
    cout << "First year result : " << firstYearScore << "% -> " << firstYearWeightingResult << "/10\n";
    cout << "Second year result : " << secondYearScore << "% -> " << secondYearWeightingResult << "/30\n";
    cout << "Third year result : " << thirdYearScore << "% -> " << thirdYearWeightingResult << "/60\n";
