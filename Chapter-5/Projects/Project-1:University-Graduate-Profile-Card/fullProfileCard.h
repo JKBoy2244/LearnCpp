e01fb199b5be8a3cdf40b26d0d7b5271c4569ade
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 23:09:31 +0100

    Create fullProfileCard.h

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.h
@@ -0,0 +1,7 @@
+#ifndef FULLPROFILECARD
+#define FULLPROFILECARD
+
+void fullProfileCard(string_view firstNameInput, string_view lastNameInput, string_view universityNameInput, string_view courseNameInput, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
+double secondYearWeightingResult, double thirdYearWeightingResult, double finalMark, string finalGrade, int internshipNumber);
+
+#endif
