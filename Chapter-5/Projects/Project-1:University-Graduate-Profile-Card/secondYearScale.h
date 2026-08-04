3d5cf48074d3b5583ae5ad032a64066689927089
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 22:33:05 +0100

    Create secondYearScale.h

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/secondYearScale.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "secondYearScale.h"
+
+double secondYearScale(double secondYearScore) {
+
+  constexpr double secondYearWeighting {0.1};
+  double secondYearWeightingResult {secondYearScore * secondYearWeighting};                             //This calculation is used as second year accounts 30% of the whole course final grade
+  return secondYearWeightingResult;
+}
