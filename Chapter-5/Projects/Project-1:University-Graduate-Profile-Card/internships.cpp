a1932441705dc1187d7f0a2e6c1614515447e9e8
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 22:55:22 +0100

    Create internships.cpp

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/internships.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "internships.h"
+
+int internships() {
+
+  cout << "How many internships did you complete during your 3 years of undergraduate at university?\n";
+  int internshipCount {};
+  cin >> internshipCount;
+  const int internshipNumber = internshipCount;
+
+  if (internshipNumber < 0) {
+    return 0;
+  }
+
+  return internshipNumber;
+}
