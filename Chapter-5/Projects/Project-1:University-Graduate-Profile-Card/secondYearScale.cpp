c6e99493237da526256df0a0404905c6785e7917
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 22:38:09 +0100

    Update secondYearScale.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/secondYearScale.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/secondYearScale.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 double secondYearScale(double secondYearScore) {
 
-  constexpr double secondYearWeighting {0.1};
+  constexpr double secondYearWeighting {0.3};
   double secondYearWeightingResult {secondYearScore * secondYearWeighting};                             //This calculation is used as second year accounts 30% of the whole course final grade
   return secondYearWeightingResult;
 }
