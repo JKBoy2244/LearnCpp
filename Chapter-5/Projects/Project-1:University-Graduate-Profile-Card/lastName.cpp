8cbca44db6086cc3c9c67aa48bbb5dd85cf4a882
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 21:56:30 +0100

    Create lastName.cpp

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/lastName.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>;
+using namespace std;
+
+#include "lastName.h";
+
+string lastName() {
+
+  cout << "Tell me your last name (surname)?\n";            //Prompts user to type last name?
+  string last {};
+  cin >> last;
+  return last;
+}
