34f1780e88e3abb70d65ad4c0209cac39113d5e0
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 23:11:05 +0100

    Update main.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/main.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/main.cpp
@@ -15,6 +15,8 @@ using namespace std;
 #include "finalScore.h"
 #include "classification.h"
 #include "internships.h"
+#include "fullProfileCard.h"
+
 
 string firstName() {
 
@@ -162,7 +164,7 @@ string classification(double finalMark) {
 
 int internships() {
 
-  cout << "How many internships did you complete during your 3 years of undergraduate at university?\n";
+  cout << "How many internships did you complete during your 3 years of undergraduate at university?\n";   //Prompts user to display number of completed internships
   int internshipCount {};
   cin >> internshipCount;
   const int internshipNumber = internshipCount;
@@ -174,4 +176,27 @@ int internships() {
   return internshipNumber;
 }
 
+void fullProfileCard(string_view firstNameInput, string_view lastNameInput, string_view universityNameInput, string_view courseNameInput, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
+double secondYearWeightingResult, double thirdYearWeightingResult, double finalMark, string finalGrade, int internshipNumber) {
+
+   cout << "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+   cout << "UNIVERSITY STUDENT GRADUATE PROFILE CARD!\n";
+   cout << "-------------------------------------------";
+   cout << "First name : " << firstNameInput << endl;
+   cout << "Last name : " << lastNameInput << endl;
+   cout << "University name : " << universityNameInput << endl;
+   cout << "Course name : " << courseNameInput << endl;
+   cout << "First year result : " << firstYearScore << "% -> " << firstYearWeightingResult << "/10\n";
+   cout << "Second year result : " << secondYearScore << "% -> " << secondYearWeightingResult << "/30\n";
+   cout << "Third year result : " << thirdYearScore << "% -> " << thirdYearWeightingResult << "/60\n";
+   cout << "Final Score : " << finalMark << "% -> " << finalGrade << endl;
+   cout << "Number of internships completed : " << internshipNumber << endl;
+   cout << "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+}
+
+int main() {
+
+  
+  return 0;
+}
 
