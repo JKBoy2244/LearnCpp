b46b420c842959010adc4a88fe783462eb45e8dc
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 22:16:35 +0100

    Create firstYearResult.cpp

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstYearResult.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "firstYearResult.h";
+
+double firstYearResult() {
+
+  cout << "As a percentage to 2 d.p, what was your first year score out of 100?\n";
+  double firstYearScore {};
+  cin >> firstYearScore;
+  
+  if (firstYearScore <= 0.00) {                                                             //Asks user for first year result to calculate the weighting score for that year
+    return 0;
+  }
+  else if (firstYearScore > 100.00) {
+    return 0;
+  }
+
+  return firstYearScore;
+}
