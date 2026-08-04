5a511e5d50637ae7fcd51981c9156f5942a23dd5
JKBoy2244 <jobayer2006@gmail.com>
2026-08-04 22:35:55 +0100

    Create thirdYearScale.cpp

---
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/thirdYearScale.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "thirdYearScale.h"
+
+double thirdYearScale(double thirdYearScore) {
+
+  constexpr double thirdYearWeighting {0.1};
+  double thirdYearWeightingResult {thirdYearScore * thirdYearWeighting};                           //This calculation is used as third year accounts 60% of the whole course final grade
+  return thirdYearWeightingResult;
+}
