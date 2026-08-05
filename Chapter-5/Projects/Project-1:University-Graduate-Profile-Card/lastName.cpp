4a0ce6fee791f6e28b80fcdcc0936a9903d75fb7
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:28:19 +0100

    Update lastName.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/lastName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/lastName.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <string_view>
 using namespace std;
 
 #include "lastName.h"
@@ -10,6 +9,5 @@ string lastName() {
   cout << "Tell me your last name (surname)?\n";            //Prompts user to type last name?
   string last {};
   cin >> last;
-  string_view lastNameInput = last;
-  return lastNameInput;
+  return last;
 }
