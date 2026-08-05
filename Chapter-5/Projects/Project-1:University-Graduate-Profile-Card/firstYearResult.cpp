4bd126e363f7695e231dec08f2ca6b85d13ac8c2
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:17:36 +0100

    Update firstYearResult.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstYearResult.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstYearResult.cpp
@@ -9,7 +9,7 @@ double firstYearResult() {
   cout << "As a percentage to 2 d.p, what was your first year score out of 100?\n";
   double firstYearScore {};
   cin >> firstYearScore;
-  bool firstValid { firstYearScore >= 0.00 and firstYearScore <= 100.00);
+  bool firstValid { firstYearScore >= 0.00 and firstYearScore <= 100.00};
 
   if (!firstValid) {                                      //Asks user for first year result to calculate the weighting score for that year
     return 0.00;
