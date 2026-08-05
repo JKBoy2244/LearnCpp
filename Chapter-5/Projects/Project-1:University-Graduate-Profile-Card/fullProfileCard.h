9432e077086ddcb18535445619188ca91a6c7602
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:32:07 +0100

    Update fullProfileCard.h

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.h
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.h
@@ -3,7 +3,7 @@
 #include <string>
 #include <string_view>
 
-void fullProfileCard(std::string_view firstNameInput, std::string_view lastNameInput, std::string_view universityNameInput, std::string_view courseNameInput, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
+void fullProfileCard(std::string firstNameInput, std::string lastNameInput, std::string universityNameInput, std::string courseNameInput, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
 double secondYearWeightingResult, double thirdYearWeightingResult, double finalMark, std::string finalGrade, int internshipNumber);
 
 #endif
