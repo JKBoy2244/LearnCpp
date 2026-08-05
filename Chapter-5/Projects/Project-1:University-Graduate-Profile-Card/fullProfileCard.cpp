91d2ac2e39c0b59b591d3307d0288d972c3a88c0
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:29:15 +0100

    Update fullProfileCard.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/fullProfileCard.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 #include "fullProfileCard.h"
 
-void fullProfileCard(string_view firstNameInput, string_view lastNameInput, string_view universityNameInput, string_view courseNameInput, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
+void fullProfileCard(string first, string last, string university, string course, double firstYearScore, double secondYearScore, double thirdYearScore, double firstYearWeightingResult,
 double secondYearWeightingResult, double thirdYearWeightingResult, double finalMark, string_view finalGrade, int internshipNumber) {
 
    cout << "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
