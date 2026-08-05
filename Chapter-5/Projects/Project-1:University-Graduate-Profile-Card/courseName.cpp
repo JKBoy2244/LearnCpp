34b098e5d4fdc4f83395021eba291385f993f355
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:27:50 +0100

    Update courseName.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/courseName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/courseName.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <string_view>
 using namespace std;
 
 #include "courseName.h"
@@ -10,6 +9,5 @@ string courseName() {
   cout << "What course did you study?\n";            //Prompts user to type course name?
   string course {};
   cin >> course;
-  string_view courseNameInput = course;
-  return courseNameInput;
+  return course;
 }
