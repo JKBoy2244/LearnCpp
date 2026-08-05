6b985bf7ccf6f6787690c24b136a1315d374fc79
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:20:16 +0100

    Change return type to string_view for first name

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/firstName.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string_view>
 using namespace std;
 
 #include "firstName.h"
@@ -9,6 +10,6 @@ string firstName() {
   cout << "Tell me your first name?\n";      //Prompts user to type first name?
   string first {};
   cin >> first;
-  //string_view firstNameInput = first;
-  return first;
+  string_view firstNameInput = first;
+  return firstNameInput;
 }
