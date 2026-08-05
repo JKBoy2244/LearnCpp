30b73de3d0318305bb2b586951988dc2355009ad
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:12:15 +0100

    Update secondYearResult.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/secondYearResult.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/secondYearResult.cpp
@@ -9,12 +9,10 @@ double secondYearResult() {
   cout << "As a percentage to 2 d.p, what was your second year score out of 100?\n";
   double secondYearScore {};
   cin >> secondYearScore;
-  
-  if (secondYearScore <= 0.00) {                                                             //Asks user for second year result to calculate the weighting score for that year
-    return 0;
-  }
-  else if (secondYearScore > 100.00) {
-    return 0;
+  bool secondValid { secondYearScore >= 0.00 and secondYearScore <= 100.00);
+
+  if (!secondValid) {                                      //Asks user for first year result to calculate the weighting score for that year
+    return 0.00;
   }
 
   return secondYearScore;
