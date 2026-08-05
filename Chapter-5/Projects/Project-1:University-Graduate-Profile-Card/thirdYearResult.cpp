54f8abd4f2c8014db2b9775521755b7ef008d589
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 12:18:00 +0100

    Update thirdYearResult.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/thirdYearResult.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/thirdYearResult.cpp
@@ -9,7 +9,7 @@ double thirdYearResult() {
   cout << "As a percentage to 2 d.p, what was your third year score out of 100?\n";
   double thirdYearScore {};
   cin >> thirdYearScore;
-  bool thirdValid { thirdYearScore >= 0.00 and thirdYearScore <= 100.00);
+  bool thirdValid { thirdYearScore >= 0.00 and thirdYearScore <= 100.00};
 
   if (!thirdValid) {                                      //Asks user for third year result to calculate the weighting score for that year
     return 0.00;
