3dffb01cee763b840adc11eb3a49503a7dad38fd
JKBoy2244 <jobayer2006@gmail.com>
2026-08-05 14:49:00 +0100

    Update courseName.cpp

--- Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/courseName.cpp
+++ Chapter-5/Projects/Project-1:University-Graduate-Profile-Card/courseName.cpp
@@ -8,6 +8,6 @@ string courseName() {
 
   cout << "What course did you study?\n";            //Prompts user to type course name?
   string course {};
-  getline(cin, course);
+  getline(cin >> ws, course);
   return course;
 }
