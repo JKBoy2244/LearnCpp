e2ed9df8ff17456c5ea43e8b3db8e403fefa67c0
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 17:54:19 +0100

    Add question five for capital cities quiz

---
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionFive.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "questionFive.h"
+
+int questionFive(int currentScore) {
+
+  cout << "Question Five: What is the capital city of Bangladesh?\n";
+  cout << "a). Islamabad b). Dhaka c). New Delhi d). Astana\n";
+  char answerOption {};
+  cin >> answerOption;
+  string correctOrNot {(answerOption == 'b') ? "correct" : "incorrect"};               //Prompts user to answer questionFive
+  
+  if (correctOrNot == "correct") {
+    cout << "Yes, b). Dhaka is the correct answer!\n";
+    currentScore++;
+    return currentScore;
+    
+  } else {
+    cout << "Sorry, your answer is incorrect, moving on to question Four!\n";
+    return currentScore;
+  }
+}
