99d5ea22e85783b83c13b3511458571711bc29b5
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 17:33:44 +0100

    Add questionOne function for capital cities quiz
    Implement question one of the capital cities quiz.

---
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionOne.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "questionOne.h"
+
+int questionOne() {
+
+  int currentScore {0};
+  
+  cout << "Question One: What is the capital city of England?\n";
+  cout << "a). Dublin b). London c). Vienna d). Athens\n";
+  char answerOption {};
+  cin >> answerOption;
+  string correctOrNot {(answerOption == 'b') ? "correct" : "incorrect"};
+  
+  if (correctOrNot == "correct") {
+    cout << "Yes, b). London is the correct answer!";
+    currentScore++;
+    return currentScore;
+    
+  } else {
+    cout << "Yes, b). London is the correct answer!";
+    return currentScore;
+  }
+}
