7d60bb1f34ea8e0078094e46dbe998381d39a60f
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 18:45:38 +0100

    Create scoreDisplay.h

---
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/scoreDisplay.h
@@ -0,0 +1,8 @@
+#ifndef SCOREDISPLAY
+#define SCOREDISPLAY
+#include <string>
+#include <string_view>
+
+void scoreDisplay(int currentScore, double percentage, std::string_view classifiedRating);
+
+#endif
