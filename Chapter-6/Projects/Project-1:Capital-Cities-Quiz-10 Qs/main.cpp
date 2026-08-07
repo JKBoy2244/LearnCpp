4aec89f2ed431c16a2931d5e62f1dba90bf45aaf
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 18:37:15 +0100

    Update main.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/main.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/main.cpp
@@ -209,8 +209,61 @@ int questionTen(int currentScore) {
 
 double percentageScoreCalculate(int currentScore) {
 
-  double percentage = ((static_cast<double>(currentScore)) / 10) * 100;
+  double percentage = ((static_cast<double>(currentScore)) / 10) * 100;          //Calculates score to a percentage
   return percentage;
+}
+
+string_view classification(double percentage) {
+
+  bool excellent {percentage >= 90.0 and percentage <= 100.0}
+  bool veryGood {percentage >= 80.0 and percentage < 90.0}
+  bool good {percentage >= 70.0 and percentage < 80.0}
+  bool average {percentage >= 60.0 and percentage < 70.0}
+  bool mediocre {percentage >= 50.0 and percentage < 60.0}                            //Converts the score into performance rating
+  bool poor {percentage >= 40.0 and percentage < 50.0}
+  bool veryPoor {percentage < 40.0}
+
+  if (excellent) {
+
+    string_view classifiedRating =  "Excellent";
+    return classifiedRating;
+  } 
+
+  else if (veryGood) {
+
+    string_view classifiedRating =  "Very Good";
+    return classifiedRating;
+  }
+
+  else if (good) {
+
+    string_view classifiedRating =  "Good";
+    return classifiedRating;
+  }
+
+  else if (average) {
+
+    string_view classifiedRating =  "Average";
+    return classifiedRating;
+  }
+
+  else if (mediocre) {
+
+    string_view classifiedRating =  "Mediocre";
+    return classifiedRating;
+  }
+
+  else if (poor) {
+
+    string_view classifiedRating =  "Poor";
+    return classifiedRating;
+  }
+
+  else if (veryPoor) {
+
+    string_view classifiedRating =  "Very Poor";
+    return classifiedRating;
+  }
 }
 
 int main() {
@@ -234,6 +287,7 @@ int main() {
     int score = questionNine(score);
     int score = questionTen(score);
     double percent = percentageScoreCalculate(score);
+    string_view performance = classification(percent);
     
   } else {
 
