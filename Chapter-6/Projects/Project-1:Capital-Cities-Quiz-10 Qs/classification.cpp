c9bde77ecff8d1a3f0f3d3877a27806e837e34ec
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 18:48:43 +0100

    Update classification.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/classification.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/classification.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 string_view classification(double percentage) {
 
-  bool excellent {percentage >= 90.0 and percentage <= 100.0}
-  bool veryGood {percentage >= 80.0 and percentage < 90.0}
-  bool good {percentage >= 70.0 and percentage < 80.0}
-  bool average {percentage >= 60.0 and percentage < 70.0}
-  bool mediocre {percentage >= 50.0 and percentage < 60.0}                            //Converts the score into performance rating
-  bool poor {percentage >= 40.0 and percentage < 50.0}
-  bool veryPoor {percentage < 40.0}
+  bool excellent {percentage >= 90.0 and percentage <= 100.0};
+  bool veryGood {percentage >= 80.0 and percentage < 90.0};
+  bool good {percentage >= 70.0 and percentage < 80.0};
+  bool average {percentage >= 60.0 and percentage < 70.0};
+  bool mediocre {percentage >= 50.0 and percentage < 60.0};                         //Converts the score into performance rating
+  bool poor {percentage >= 40.0 and percentage < 50.0};
+  bool veryPoor {percentage < 40.0};
 
   if (excellent) {
 
