0ecb322dab84fcf2e9f11dcf15bf56f8dcd7b1b4
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 18:21:23 +0100

    Update questionTen.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionTen.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionTen.cpp
@@ -9,7 +9,7 @@ int questionTen(int currentScore) {
   cout << "Question Ten: What is the capital city of Dominican Republic?\n";
   cout << "a). Santo Domingo b). Saint George's c). San Marino d). Saint John's\n";
   char answerOption {};
-  getline(cin >> ws, answerOption);
+  getline(cin >> ws, answerOption);                                          //This answer requires more than one word and with just the string type, it only takes the first word even when you type more than one word, hence getline was used
   string correctOrNot {(answerOption == 'a') ? "correct" : "incorrect"};               //Prompts user to answer questionTen
   
   if (correctOrNot == "correct") {
