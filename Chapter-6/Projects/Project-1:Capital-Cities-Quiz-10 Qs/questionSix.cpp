326beaa88189c01cd4cda689c9c362a4ac56a294
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 17:57:39 +0100

    Add includes and questionSix function

---
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionSix.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "questionSix.h"
+
+int questionSix(int currentScore) {
+
+  cout << "Question Five: What is the capital city of South Korea?\n";
+  cout << "a). Pyongyang b). Beijing c). Seoul d). Kuala Lumpur\n";
+  char answerOption {};
+  cin >> answerOption;
+  string correctOrNot {(answerOption == 'c') ? "correct" : "incorrect"};               //Prompts user to answer questionFive
+  
+  if (correctOrNot == "correct") {
+    cout << "Yes, c). Seoul is the correct answer!\n";
+    currentScore++;
+    return currentScore;
+    
+  } else {
+    cout << "Sorry, your answer is incorrect, moving on to question Seven!\n";
+    return currentScore;
+  }
+}
