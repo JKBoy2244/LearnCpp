f8009f86a9eff5766ff58ed7d8ae9900f289c9ed
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 19:14:15 +0100

    Update scoreDisplay.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/scoreDisplay.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/scoreDisplay.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 #include "scoreDisplay.h"
 
-void scoreDisplay(int currentScore, double percentage, string_view classifiedRating) {
+void scoreDisplay(int currentScore, double percentage, string_view classifiedRating, string feedBack) {
 
   cout << "---------------------------------------------------------------------------------------\n";              //Displays the score statistics card
   cout << "Total score for this quiz: " << currentScore << "/10\n";
   cout << "Percentage: " << percentage << "%\n";
   cout << "Performance: " << classifiedRating << endl;
+  cout << "Feedback: " << feedBack << endl;
   cout << "---------------------------------------------------------------------------------------\n";
 }
