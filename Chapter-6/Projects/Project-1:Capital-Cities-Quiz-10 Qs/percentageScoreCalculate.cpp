a031a3601bd4ec3f28901e1b9166f1010e06751e
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 18:26:08 +0100

    Include header file in percentageScoreCalculate.cpp
    Added header file inclusion for percentage score calculation.

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/percentageScoreCalculate.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/percentageScoreCalculate.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+#include "percentageScoreCalculate.h"
+
 double percentageScoreCalculate(int currentScore) {
 
   double percentage = ((static_cast<double>(currentScore)) / 10) * 100;
