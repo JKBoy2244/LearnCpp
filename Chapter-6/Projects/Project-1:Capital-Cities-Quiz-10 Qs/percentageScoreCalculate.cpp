c239b51c1961165367b8d238bdc8cc6987bc0e69
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 18:36:51 +0100

    Update percentageScoreCalculate.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/percentageScoreCalculate.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/percentageScoreCalculate.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 double percentageScoreCalculate(int currentScore) {
 
-  double percentage = ((static_cast<double>(currentScore)) / 10) * 100;
+  double percentage = ((static_cast<double>(currentScore)) / 10) * 100;        //Calculates score to a percentage
   return percentage;
 }
