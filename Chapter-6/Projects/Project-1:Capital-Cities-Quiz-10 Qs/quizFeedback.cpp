9ebfbffec2cfb0148e19ca26eba2c6ca0e8e11be
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 19:15:54 +0100

    Update quizFeedback.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/quizFeedback.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/quizFeedback.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 string quizFeedback() {
 
-    cout << "In a few senterences or few words if you want, tell me how you found this quiz overall? It's optional, so if you don't want to answer, it's fine, just write null or none or something like that???\n";
+    cout << "In a few senterences or few words if you want, tell me how you found this quiz overall? It's optional, so if you don't want to answer, it's fine, just write null or none or something like that?\n";
     string feedBack {};
     getLine(cin >> ws, feedBack);                       //Optional feedback (getline is used as if using cin only, the program only takes the first word ignoring the remaining words for a more than one word response)
     return feedBack;
