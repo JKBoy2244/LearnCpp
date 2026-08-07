6f7e018c60e8037f5ff94bca3e042762e7daad76
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 17:44:49 +0100

    Update questionTwo.cpp

--- Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionTwo.cpp
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionTwo.cpp
@@ -18,7 +18,7 @@ int questionTwo(int currentScore) {
     return currentScore;
     
   } else {
-    cout << "Yes, b). London is the correct answer!\n";
+    cout << "Sorry, your answer is incorrect, moving on to question Three!\n";
     return currentScore;
   }
 }
