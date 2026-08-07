e298d22c12da55d83e295ce3d2e2a1c9daac7d45
JKBoy2244 <jobayer2006@gmail.com>
2026-08-07 17:42:45 +0100

    Implement questionTwo for capital city quiz
    Adds questionTwo function to quiz for capital cities.

---
+++ Chapter-6/Projects/Project-1:Capital-Cities-Quiz-10 Qs/questionTwo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "questionTwo.h"
+
+int questionTwo(int currentScore) {
+
+  cout << "Question One: What is the capital city of Denmark?\n";
+  cout << "a). Copenhagen b). Cairo c). Paris d). Mexico City\n";
+  char answerOption {};
+  cin >> answerOption;
+  string correctOrNot {(answerOption == 'a') ? "correct" : "incorrect"};               //Prompts user to answer questionTwo
+  
+  if (correctOrNot == "correct") {
+    cout << "Yes, a). Copenhagen is the correct answer!";
+    currentScore++;
+    return currentScore;
+    
+  } else {
+    cout << "Yes, b). London is the correct answer!";
+    return currentScore;
+  }
+}
