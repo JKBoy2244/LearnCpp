710664804c5fd078b1940374499b5ce9837a8c97
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 18:08:57 +0100

    Update month.cpp

--- Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/month.cpp
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/month.cpp
@@ -8,6 +8,6 @@ int month() {
 
   cout << "Please enter a month number from 1 to 12 as a second part of the date?\n";                                             //Prompts user to enter the month
   int monthDate {};
-  cin monthDate;
+  cin >> monthDate;
   return monthDate;
 }
