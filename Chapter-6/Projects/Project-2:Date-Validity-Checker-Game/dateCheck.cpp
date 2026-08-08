c5bb01f1aefa31325582f0106bf7d1d36a4b20d9
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 17:47:09 +0100

    Update dateCheck.cpp

--- Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <string>
 #include <string_view>
 using namespace std;
 
 #include "dateCheck.h"
 
-string_view dateCheck(int day, string month, int year) {
+string_view dateCheck(int day, int month, int year) {
 
   bool thirtyDayRange {((month == 4) || (month == 6) || (month == 9) || (month == 12)) && (year >= 1950 && year <= 2050) && (day >= 1 && day <= 30)}                                                             //Conditions list for dates
   bool thirtyOneDayRange {((month == 1) || (month == 3) || (month == 5) || (month == 7) || (month == 8) || (month == 10) || (month == 11)) && (year >= 1950 && year <= 2050) && (day >= 1 && day <= 31)}
