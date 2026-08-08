46393375ce0b4126b5b33a45a95ba5a8c9bac77a
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 18:18:31 +0100

    Update dateCheck.cpp

--- Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 string_view dateCheck(int dayDate, int monthDate, int yearDate) {
 
-  constexpr int minYear = 1950
-  constexpr int maxYear = 2050
+  constexpr int minYear = 1950;
+  constexpr int maxYear = 2050;
   
   bool thirtyDayRange {((monthDate == 4) || (monthDate == 6) || (monthDate == 9) || (monthDate == 11)) && (yearDate >= minYear && yearDate <= maxYear) && (dayDate >= 1 && dayDate <= 30)};                                                             //Conditions list for dates
   bool thirtyOneDayRange {((monthDate == 1) || (monthDate == 3) || (monthDate == 5) || (monthDate == 7) || (monthDate == 8) || (monthDate == 10) || (monthDate == 12)) && (yearDate >= minYear && yearDate <= maxYear) && (dayDate >= 1 && dayDate <= 31)};
