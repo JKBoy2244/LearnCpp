cc72bbca5f65a2454d7e14bcfa411eb5eaa69165
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 18:19:41 +0100

    Update dateCheck.cpp

--- Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
@@ -11,7 +11,7 @@ string_view dateCheck(int dayDate, int monthDate, int yearDate) {
   
   bool thirtyDayRange {((monthDate == 4) || (monthDate == 6) || (monthDate == 9) || (monthDate == 11)) && (yearDate >= minYear && yearDate <= maxYear) && (dayDate >= 1 && dayDate <= 30)};                                                             //Conditions list for dates
   bool thirtyOneDayRange {((monthDate == 1) || (monthDate == 3) || (monthDate == 5) || (monthDate == 7) || (monthDate == 8) || (monthDate == 10) || (monthDate == 12)) && (yearDate >= minYear && yearDate <= maxYear) && (dayDate >= 1 && dayDate <= 31)};
-  bool februaryRange {(yearDate % 400 == 0) || ((yearDate % 4 == 0) && (yearDate % 100 != 0)) ? ((monthDate == 2) && (yearDate >= minYear && yearDate <= 2050) && (dayDate >= 1 && day <= 29)) : ((monthDate == 2) && (yearDate >= 1950 && yearDate <= maxYear) && 
+  bool februaryRange {(yearDate % 400 == 0) || ((yearDate % 4 == 0) && (yearDate % 100 != 0)) ? ((monthDate == 2) && (yearDate >= minYear && yearDate <= 2050) && (dayDate >= 1 && dayDate <= 29)) : ((monthDate == 2) && (yearDate >= 1950 && yearDate <= maxYear) && 
                       (dayDate >= 1 && dayDate <= 28))};
 
   if ((thirtyDayRange) || (thirtyOneDayRange) || (februaryRange)) {
