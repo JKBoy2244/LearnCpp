99a58b6d247e051642a92ea63ee7a410bc6f309d
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 18:18:24 +0100

    Update dateCheck.cpp

--- Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/dateCheck.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 string_view dateCheck(int dayDate, int monthDate, int yearDate) {
 
-  bool thirtyDayRange {((monthDate == 4) || (monthDate == 6) || (monthDate == 9) || (monthDate == 11)) && (yearDate >= 1950 && yearDate <= 2050) && (dayDate >= 1 && dayDate <= 30)};                                                             //Conditions list for dates
-  bool thirtyOneDayRange {((monthDate == 1) || (monthDate == 3) || (monthDate == 5) || (monthDate == 7) || (monthDate == 8) || (monthDate == 10) || (monthDate == 12)) && (yearDate >= 1950 && yearDate <= 2050) && (dayDate >= 1 && dayDate <= 31)};
-  bool februaryRange {(yearDate % 400 == 0) || ((yearDate % 4 == 0) && (yearDate % 100 != 0)) ? ((monthDate == 2) && (yearDate >= 1950 && yearDate <= 2050) && (dayDate >= 1 && day <= 29)) : ((monthDate == 2) && (yearDate >= 1950 && yearDate <= 2050) && 
+  constexpr int minYear = 1950
+  constexpr int maxYear = 2050
+  
+  bool thirtyDayRange {((monthDate == 4) || (monthDate == 6) || (monthDate == 9) || (monthDate == 11)) && (yearDate >= minYear && yearDate <= maxYear) && (dayDate >= 1 && dayDate <= 30)};                                                             //Conditions list for dates
+  bool thirtyOneDayRange {((monthDate == 1) || (monthDate == 3) || (monthDate == 5) || (monthDate == 7) || (monthDate == 8) || (monthDate == 10) || (monthDate == 12)) && (yearDate >= minYear && yearDate <= maxYear) && (dayDate >= 1 && dayDate <= 31)};
+  bool februaryRange {(yearDate % 400 == 0) || ((yearDate % 4 == 0) && (yearDate % 100 != 0)) ? ((monthDate == 2) && (yearDate >= minYear && yearDate <= 2050) && (dayDate >= 1 && day <= 29)) : ((monthDate == 2) && (yearDate >= 1950 && yearDate <= maxYear) && 
                       (dayDate >= 1 && dayDate <= 28))};
 
   if ((thirtyDayRange) || (thirtyOneDayRange) || (februaryRange)) {
