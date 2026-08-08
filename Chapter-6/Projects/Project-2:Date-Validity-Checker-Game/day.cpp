213dac0af021a0bd0c7eaf33b70a9808cda194ff
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 18:08:47 +0100

    Update day.cpp

--- Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/day.cpp
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/day.cpp
@@ -7,6 +7,6 @@ int day() {
 
   cout << "Please enter a day between 1 and however many days in total for a partiuclar month as a date?\n";                              //Prompts user to enter the day
   int dayDate {};
-  cin dayDate;
+  cin >> dayDate;
   return dayDate;
 }
