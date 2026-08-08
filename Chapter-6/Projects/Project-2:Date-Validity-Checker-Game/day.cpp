d0b6e027378e05bb76d4d18aef0399d794c44514
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 12:28:46 +0100

    Add day function to prompt user for date input

---
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/day.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#include "day.h"
+
+int day() {
+
+  cout << "Please enter a day between 1 and however many days in total for a partiuclar month as a date?\n";                              //Prompts user to enter the day
+  int dayDate {};
+  cin dayDate;
+  return dayDate;
+}
