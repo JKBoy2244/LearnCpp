cac2827048ffd20df80818bf20aaa3c93abb0a78
JKBoy2244 <jobayer2006@gmail.com>
2026-08-08 12:31:41 +0100

    Add year function to prompt for user input

---
+++ Chapter-6/Projects/Project-2:Date-Validity-Checker-Game/year.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#include "year.h"
+
+int year() {
+
+  cout << "Please enter a year as the third part of the date?\n";                                                              //Prompts user to enter the year
+  int yearDate {};
+  cin yearDate;
+  return yearDate;
+}
