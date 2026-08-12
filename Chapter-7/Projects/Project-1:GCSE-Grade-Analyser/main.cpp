12b013d232bf0a3aa3eaf7e0cbb383d0362b0aa0
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 13:34:47 +0100

    Update main.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/main.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/main.cpp
@@ -4,7 +4,18 @@
 using namespace std;
 
 #include "constants.h"                 //for constants::minGrade and constants::maxGrade
+#include "mathGrade.h"                 //for mathematics::mathsGrade
 
+int mathsGrade() {
+
+  cout << "Enter your GCSE mathematics grade?\n";
+  int mathGrade {};
+  cin mathGrade;
+  bool validMathGrade {mathGrade >= constants::minGrade && mathGrade <= constants::maxGrade};
+  int finalMathGrade {(validMathGrade) ? mathGrade : 0};
+  return finalMathGrade;
+
+}
 
 int main() {
 
