160c6895a7ba7f6d647debd6ff5fdaba1415fc6f
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 21:45:36 +0100

    Create tripleScienceTotal.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/tripleScienceTotal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "tripleScienceTotal.h"
+
+namespace tripleTotalSubjects {
+  int tripleScienceTotal(int finalCount) {
+
+    [[maybe_unused]] int totalSubjectsTriple {5 + finalCount};
+    return totalSubjectsTriple;
+
+  }
+}
