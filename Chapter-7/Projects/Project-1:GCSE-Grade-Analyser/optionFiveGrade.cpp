d8f6c1d720d4e9c51316acde443fb7bec084a070
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 23:39:05 +0100

    Create optionFiveGrade.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionFiveGrade.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+namespace std;
+
+#include "constants.h"
+#include "optionSubjectFive.h" 
+
+namespace fiveOption {
+  int optionFiveGrade(string optionFive) {
+
+    cout << "Enter your GCSE " << optionFive << " grade?\n"; 
+    [[maybe_unused]] int optFiveGrade {};
+    cin >> optFiveGrade;
+    bool validOptFiveGrade {optFiveGrade >= constants::minGrade && optFiveGrade <= constants::maxGrade};
+    int finalOptFiveGrade {(validOptFiveGrade) ? optFiveGrade : 0};
+    return finalOptFiveGrade; 
+
+ }
+}
