75b479e17fc4902c98cce9601c2e537899525818
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 12:59:26 +0100

    Update and rename main.cpp to main.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grades-Analyser/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+
+#include "constants.h"                 //for constants::minGrade and constants::maxGrade
+
+
+int main() {
+
+  return 0;
+}
