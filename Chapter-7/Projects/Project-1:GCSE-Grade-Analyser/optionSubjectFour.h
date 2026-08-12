f82f1badee4c0bb2ee446df9cc41395d9a798f4d
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 22:50:09 +0100

    Add header file for optionSubjectFour functionality

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectFour.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONSUBJECTFOUR
+#define OPTIONSUBJECTFOUR
+#include <string>
+
+namespace optionFour {
+
+  std::string optionSubjectFour(std::string optionOne, std::string optionTwo, std::string optionThree);
+}
+
+#endif
