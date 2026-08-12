828e880a5a841227bddcad025a7ec1ec23daab5b
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 23:28:22 +0100

    Create optionOneGrade.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionOneGrade.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "constants.h"
+#include "optionOneGrade.h"
+
+namespace oneOption {
+  int optionOneGrade(string optionOne) {
+
+    cout << "Enter your GCSE " << optionOne << " grade?\n"; 
+    [[maybe_unused]] int optOneGrade {};
+    cin >> optOneGrade;
+    bool validOptOneGrade {optOneGrade >= constants::minGrade && optOneGrade <= constants::maxGrade};
+    int finalOptOneGrade {(validOptOneGrade) ? optOneGrade : 0};
+    return finalOptOneGrade;
+
+  }
+}
