30b29e01b17006e63a8c55df8d72b2684c94dcb6
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 22:49:18 +0100

    Add header file for option subject three

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectThree.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONSUBJECTTHREE
+#define OPTIONSUBJECTTHREE
+#include <string>
+
+namespace optionThree {
+
+  std::string optionSubjectThree(std::string optionOne, std::string optionTwo);
+}
+
+#endif
