634142197f0992c46aef5fd97e537442a437ac74
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 21:44:55 +0100

    Create combinedScienceTotal.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/combinedScienceTotal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "combinedScienceTotal.h"
+
+namespace combinedTotalSubjects {
+  int combinedScienceTotal(int finalCount) {
+
+    [[maybe_unused]] int totalSubjectsCombined {5 + finalCount};
+    return totalSubjectsCombined;
+
+  }
+}
