d0453e36849e03daa4cf01f752d1569e1e9cc3dc
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 21:32:48 +0100

    Create optionalCount.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionalCount.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "constants.h"
+#include "optionalCount.h"
+
+namespace Option {
+  int optionalCount() {
+
+    cout << "How many option choices are you taking for GCSEs(maximum 6)?\n"
+    const unsigned int optionCount {};
+    cin >> optionCount;
+    bool validCount {optionCount >= constants::minOptionCount && optionCount <= constants::maxOptionCount};
+    int finalCount {(optionCount) ? optionCount : 0};
+    return finalCount;
+
+  } 
+}
