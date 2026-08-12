8eaebab664522314f2a1d60cd894afaed97446e8
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 13:33:48 +0100

    Create mathGrade.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/mathGrade.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+#include "mathGrade.h"  //for mathematics::mathsGrade
+
+int mathsGrade() {
+
+  cout << "Enter your GCSE mathematics grade?\n";
+  int mathGrade {};
+  cin mathGrade;
+  bool validMathGrade {mathGrade >= constants::minGrade && mathGrade <= constants::maxGrade};
+  int finalMathGrade {(validMathGrade) ? mathGrade : 0};
+  return finalMathGrade;
+
+}
