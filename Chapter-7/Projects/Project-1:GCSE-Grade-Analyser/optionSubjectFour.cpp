14380747f5f8a1c7e6fef9bd990822a2d60b266f
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 22:55:59 +0100

    Add optionSubjectFour function for subject validation
    Implement optionSubjectFour function to validate optional subject input.

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectFour.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "optionSubjectFour.h"
+
+namespace optionFour {
+  string optionSubjectFour(string optionOne, string optionTwo, string optionThree) {
+
+    cout << "Enter valid optional subject four which isn't the core subjects maths, english and science and actually exists and when typing, all lower cases please with no single upper case and if your subject
+           isn't there, write other (all lower case too)?\n";
+    [[maybe_unused]] string optionFour {};
+    getline(cin >> ws, optionFour);
+    bool validOptionFour {(optionFour == "geography") || (optionFour == "history") || (optionFour == "art") || (optionFour == "french") || (optionFour == "spanish") || (optionFour == "german") 
+                       || (optionFour == "italian") || (optionFour == "chinese mandarin") || (optionFour == "religious studies") || (optionFour == "latin") || (optionFour == "computer science")
+                       || (optionFour == "statistics") || (optionFour == "design & technology") || (optionFour == "physical education") || (optionFour == "other")}
+
+    if (((validOptionFour) && (optionFour != optionThree) && (optionFour != optionTwo) && (optionFour != optionOne)) || (optionFour == "other")) {
+      return optionFour;
+    
+    } else {
+
+      return " ";
+    }
+  }
+}
