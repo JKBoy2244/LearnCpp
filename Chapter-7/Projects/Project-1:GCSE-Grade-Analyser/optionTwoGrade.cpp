ccc914f2ed7c791c05982e8370bdc4535b60cf4a
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 23:31:34 +0100

    Create optionTwoGrade.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionTwoGrade.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "constants.h"
+#include "optionTwoGrade.h"
+
+namespace twoOption {
+
+  int optionTwoGrade(string optionTwo) {
+
+    cout << "Enter your GCSE " << optionTwo << " grade?\n"; 
+    [[maybe_unused]] int optOTwoGrade {};
+    cin >> optTwoGrade;
+    bool validOptTwoGrade {optTwoGrade >= constants::minGrade && optTwoGrade <= constants::maxGrade};
+    int finalOptTwoGrade {(validOptTwoGrade) ? optTwoGrade : 0};
+    return finalOptTwoGrade; 
+
+ }
+}
