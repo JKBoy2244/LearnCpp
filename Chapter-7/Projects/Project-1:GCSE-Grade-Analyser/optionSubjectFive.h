3020206aad2f4ce2aa3c35cf1a3102d1e3ad3025
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 22:51:06 +0100

    Add header file for optionSubjectFive function

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectFive.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONSUBJECTFIVE
+#define OPTIONSUBJECTFIVE
+#include <string>
+
+namespace optionFive {
+
+  std::string optionSubjectFive(std::string optionOne, std::string optionTwo, std::string optionThree, std::string optionFour);
+}
+
+#endif
