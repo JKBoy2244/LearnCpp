ea246a9d6e0f5b6e1070b4531fb69eab5a2308dd
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 23:38:05 +0100

    Create optionFourGrade.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionFourGrade.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+namespace std;
+
+#include "constants.h"
+#include "optionSubjectFour.h" 
+
+namespace fourOption {
+  int optionFourGrade(string optionFour) {
+
+    cout << "Enter your GCSE " << optionFour << " grade?\n"; 
+    [[maybe_unused]] int optFourGrade {};
+    cin >> optFourGrade;
+    bool validOptFourGrade {optFourGrade >= constants::minGrade && optFourGrade <= constants::maxGrade};
+    int finalOptFourGrade {(validOptFourGrade) ? optFourGrade : 0};
+    return finalOptFourGrade; 
+
+  }
+}
