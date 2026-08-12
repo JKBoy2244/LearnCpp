b99b55db3660bb1478c9530bafc38acd251f5aeb
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 13:45:49 +0100

    Create englishLanguageGrade.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/englishLanguageGrade.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "constants.h"                     //for constants::minGrade and constants::maxGrade
+#include "englishLangauageGrade.h"         //for englishLanguage::englishLanguageGrade
+
+namespace englishLanguage {
+  int englishLanguageGrade() {
+
+    cout << "Enter your GCSE english language grade?\n";
+    int englishLanguageGrade {};
+    cin >> englishLanguageGrade;
+    bool validEnglishLanguageGrade {englishLanguageGrade >= constants::minGrade && englishLanguageGrade <= constants::maxGrade};
+    int finalEnglishLanguageGrade {(validEnglishLanguageGrade) ? englishLanguageGrade : 0};
+    return finalEnglishLanguage;
+
+  }
+}
