6dde58a80ad95d9ffce4ed2240b387f163c4eec4
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 14:30:14 +0100

    Update englishLiteratureGrade.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/englishLiteratureGrade.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/englishLiteratureGrade.cpp
@@ -4,13 +4,16 @@ using namespace std;
 #include "constants.h"                 //for constants::minGrade and constants::maxGrade
 #include "englishLiteratureGrade.h"
 
-int englishLiteratureGrade() {
+namespace englishLiterature {
 
-  cout << "Enter your GCSE english literature grade?\n";
-  int englishLiteratureGrade {};
-  cin >> englishLiteratureGrade;
-  bool validEnglishLiteratureGrade {englishLiteratureGrade >= constants::minGrade && mathGrade <= constants::maxGrade};
-  int finalEnglishLiteratureGrade {(validEnglishLiteratureGrade) ? englishLiteratureGrade : 0};
-  return finalEnglishLiteratureGrade;
+  int englishLiteratureGrade() {
 
+    cout << "Enter your GCSE english literature grade?\n";
+    int englishLiteratureGrade {};
+    cin >> englishLiteratureGrade;
+    bool validEnglishLiteratureGrade {englishLiteratureGrade >= constants::minGrade && mathGrade <= constants::maxGrade};
+    int finalEnglishLiteratureGrade {(validEnglishLiteratureGrade) ? englishLiteratureGrade : 0};
+    return finalEnglishLiteratureGrade;
+
+  }
 }
