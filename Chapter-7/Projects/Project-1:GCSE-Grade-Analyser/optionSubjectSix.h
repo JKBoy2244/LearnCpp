15ab82b9fb2eefcaa36e059f523c183855b4185d
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 22:51:57 +0100

    Create optionSubjectSix.h

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectSix.h
@@ -0,0 +1,10 @@
+#ifndef OPTIONSUBJECTSIX
+#define OPTIONSUBJECTSIX
+#include <string>
+
+namespace optionSix {
+
+  std::string optionSubjectSix(std::string optionOne, std::string optionTwo, std::string optionThree, std::string optionFour, std::string optionFive);
+}
+
+#endif
