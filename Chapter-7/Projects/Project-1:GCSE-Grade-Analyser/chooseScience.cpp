3449535b7c87e434a5c90306edfab8165096a595
JKBoy2244 <jobayer2006@gmail.com>
2026-08-12 14:48:24 +0100

    Create chooseScience.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/chooseScience.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "chooseScience.h"
+
+namespace ScienceChoose {
+  string chooseScience() {
+
+    cout << "Did you take triple science or combined science, type either triple or combined please (nothing else)?\n";
+    string scienceOption {};
+    cin >> scienceOption;
+    if (scienceOption == "triple" || scienceOption == "combined") {
+      return scienceOption;
+    } else {
+      return 0;
+  }
+}
