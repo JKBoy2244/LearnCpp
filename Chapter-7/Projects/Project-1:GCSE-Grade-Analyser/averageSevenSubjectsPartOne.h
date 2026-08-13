283f355be6dee781114c2e6fc955e089f48da002
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:39:49 +0100

    Add header for averageSevenSubjectsPartOne function

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSevenSubjectsPartOne.h
@@ -0,0 +1,9 @@
+#ifndef AVERAGESEVENSUBJECTSPARTONE
+#define AVERAGESEVENSUBJECTSPARTONE
+
+namespace averageSevenSubjectsOne {
+  double averageSevenSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo
+                                   , int optionOneGrade, int optionTwoGrade);
+}
+
+#endif
