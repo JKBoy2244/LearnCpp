62e3ff4bc25b374816a0731b829b7c4f04545bfe
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:42:37 +0100

    Create averageTenSubjectsPartOne.h

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageTenSubjectsPartOne.h
@@ -0,0 +1,9 @@
+#ifndef AVERAGETENSUBJECTSPARTONE
+#define AVERAGETENSUBJECTSPARTONE
+
+namespace averageTenSubjectsOne {
+  double averageTenSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo
+                                 , int optionOneGrade, int optionTwoGrade, int optionThreeGrade, int optionFourGrade, int optionFiveGrade);
+}
+
+#endif
