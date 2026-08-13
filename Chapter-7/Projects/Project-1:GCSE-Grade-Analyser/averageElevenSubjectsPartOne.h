972c15df55ef24b4c1ef62cd99ed1d36c011f006
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:43:33 +0100

    Add optionSixGrade parameter to average function

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageElevenSubjectsPartOne.h
@@ -0,0 +1,9 @@
+#ifndef AVERAGEELEVENSUBJECTSPARTONE
+#define AVERAGEELEVENSUBJECTSPARTONE
+
+namespace averageElevenSubjectsOne {
+  double averageElevenSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo
+                                    , int optionOneGrade, int optionTwoGrade, int optionThreeGrade, int optionFourGrade, int optionFiveGrade, int optionSixGrade);
+}
+
+#endif
