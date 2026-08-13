4603ccbb5846563d809ace1844552f5b22ea68e1
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 13:11:56 +0100

    Create gradeTier.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/gradeTier.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string_view>
+using namespace std;
+
+#include "gradeTier.h"
+
+namespace tier {
+  string_view gradeTier(double average) {
+
+    if (average == 9.0) {
+      string_view gradeComment == "Outstanding";
+      return gradeComment;
+    }
+
+    if (average >= 8.0 && average < 9.0) {
+      string_view gradeComment == "Very Good";
+      return gradeComment;
+    }
+
+    if (average >= 7.0 && average < 8.0) {
+      string_view gradeComment == "Good";
+      return gradeComment;
+    }
+
+    if (average >= 6.0 && average < 7.0) {
+      string_view gradeComment == "Satisfactory";
+      return gradeComment;
+    }
+
+    if (average >= 5.0 && average < 6.0) {
+      string_view gradeComment == "Not so good";
+      return gradeComment;
+    }
+
+    if (average >= 4.0 && average < 5.0) {
+      string_view gradeComment == "Bad";
+      return gradeComment;
+    }
+
+    if (average < 4.0) {
+      string_view gradeComment == "Poor";
+      return gradeComment;
+    }
+  }
+}
