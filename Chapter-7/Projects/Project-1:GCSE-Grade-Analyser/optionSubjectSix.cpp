b60a236ae1219b9c4423d5bee6da645e527e56b6
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 21:38:06 +0100

    Change optionSix to static in optionSubjectSix function
    Changed optionSix variable from non-static to static.

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectSix.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectSix.cpp
@@ -9,7 +9,7 @@ namespace optionSix {
 
     cout << "Enter valid optional subject six which isn't the core subjects maths, english and science and actually exists and when typing, all lower cases please with no single upper case and if your subject "
            "isn't there, write other (all lower case too)?\n";
-    [[maybe_unused]] string optionSix {};
+    [[maybe_unused]] static string optionSix {};
     getline(cin >> ws, optionSix);
     bool validOptionSix {(optionSix == "geography") || (optionSix == "history") || (optionSix == "art") || (optionSix == "french") || (optionSix == "spanish") || (optionSix == "german") 
                        || (optionSix == "italian") || (optionSix == "chinese mandarin") || (optionSix == "religious studies") || (optionSix == "latin") || (optionSix == "computer science")
