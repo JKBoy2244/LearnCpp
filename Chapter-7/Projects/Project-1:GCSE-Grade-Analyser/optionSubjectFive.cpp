52984f81e231dab73bd89725d5bce9ff22208f14
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 21:37:32 +0100

    Update optionSubjectFive.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectFive.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectFive.cpp
@@ -9,7 +9,7 @@ namespace optionFive {
 
     cout << "Enter valid optional subject five which isn't the core subjects maths, english and science and actually exists and when typing, all lower cases please with no single upper case and if your subject "
            "isn't there, write other (all lower case too)?\n";
-    [[maybe_unused]] string optionFive {};
+    [[maybe_unused]] static string optionFive {};
     getline(cin >> ws, optionFive);
     bool validOptionFive {(optionFive == "geography") || (optionFive == "history") || (optionFive == "art") || (optionFive == "french") || (optionFive == "spanish") || (optionFive == "german") 
                        || (optionFive == "italian") || (optionFive == "chinese mandarin") || (optionFive == "religious studies") || (optionFive == "latin") || (optionFive == "computer science")
