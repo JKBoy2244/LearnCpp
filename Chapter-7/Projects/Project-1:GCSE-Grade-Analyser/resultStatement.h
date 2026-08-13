fe3c5be87930c9bbf585a9632094e65be0f3390b
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 13:23:34 +0100

    Add resultStatement header file

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/resultStatement.h
@@ -0,0 +1,10 @@
+#ifndef RESULTSTATEMENT
+#define RESULTSTATEMENT
+#include <string_view>
+
+inline namespace result {
+
+  void resultStatement(double average, std::string_view gradeComment, int finalCount);
+}
+
+#endif
