35f59e9990ca30e5b87b56bd7f3f1dea01a68aaa
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 13:01:03 +0100

    Add header file for averageSubjectsPartTwo function

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSubjectsPartTwo.h
@@ -0,0 +1,9 @@
+#ifndef AVERAGESUBJECTSPARTTWO
+#define AVERAGESUBJECTSPARTTWO
+
+namespace averageSubjectsTwo {
+  double averageSubjectsPartTwo(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalBiologyGrade, int finalChemistryGrade
+                              int finalPhysicsGrade, int optionOneGrade, int optionTwoGrade, int optionThreeGrade, int optionFourGrade, int optionFiveGrade, int optionSixGrade);
+}
+
+#endif
