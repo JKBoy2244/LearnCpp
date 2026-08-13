aa1fb1d9b28438d94a435b385583ca657befaf7a
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 21:35:08 +0100

    Update combinedScienceGradeOne.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/combinedScienceGradeOne.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/combinedScienceGradeOne.cpp
@@ -8,7 +8,7 @@ namespace firstScienceCombinedGrade {
   int combinedScienceGradeOne() {
 
     cout << "Enter your first GCSE combined science grade?\n";
-    [[maybe_unused]] int scienceGradeOne {};
+    [[maybe_unused]] static int scienceGradeOne {};
     cin >> scienceGradeOne;
     bool validScienceGradeOne {scienceGradeOne >= constants::minGrade && scienceGradeOne <= constants::maxGrade};
     int finalScienceGradeOne {(validScienceGradeOne) ? scienceGradeOne : 0};
