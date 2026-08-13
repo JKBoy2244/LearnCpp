6d750a70d7ed876dce0e841fda973a265c9106f6
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 15:01:42 +0100

    Update optionSubjectThree.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectThree.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/optionSubjectThree.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <string>
-namespace std;
+using namespace std;
 
 #include "optionSubjectThree.h"
 
 namespace optionThree {
   string optionSubjectThree(string optionOne, string optionTwo) {
 
-    cout << "Enter valid optional subject three which isn't the core subjects maths, english and science and actually exists and when typing, all lower cases please with no single upper case and if your subject
-           isn't there, write other (all lower case too)?\n";
+    cout << "Enter valid optional subject three which isn't the core subjects maths, english and science and actually exists and when typing, all lower cases please with no single upper case and if your subject "
+           "isn't there, write other (all lower case too)?\n";
     [[maybe_unused]] string optionThree {};
     getline(cin >> ws, optionThree);
     bool validOptionThree {(optionThree == "geography") || (optionThree == "history") || (optionThree == "art") || (optionThree == "french") || (optionThree == "spanish") || (optionThree == "german") 
                        || (optionThree == "italian") || (optionThree == "chinese mandarin") || (optionThree == "religious studies") || (optionThree == "latin") || (optionThree == "computer science")
-                       || (optionThree == "statistics") || (optionThree == "design & technology") || (optionThree == "physical education") || (optionThree == "other")}
+                       || (optionThree == "statistics") || (optionThree == "design & technology") || (optionThree == "physical education") || (optionThree == "other")};
 
     if (((validOptionThree) && (optionThree != optionTwo) && (optionThree != optionOne)) || (optionThree == "other")) {
       return optionThree;
