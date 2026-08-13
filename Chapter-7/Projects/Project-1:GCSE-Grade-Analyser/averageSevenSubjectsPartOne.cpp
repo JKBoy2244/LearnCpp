88ec9bc525b4d6ff5d56654961c378a0e615c7f6
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:27:35 +0100

    Implement average calculation for seven subjects

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSevenSubjectsPartOne.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "averageSevenSubjectsPartOne.h"
+
+namespace averageSevenSubjectsOne {
+  double averageSevenSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo
+                                   , int optionOneGrade, int optionTwoGrade) {
+
+    int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo + optionOneGrade + optionTwoGrade};
+    double average { static_cast<double>(totalScore) / static_cast<double>(finalCount) };
+    return average;
+  }
+}
