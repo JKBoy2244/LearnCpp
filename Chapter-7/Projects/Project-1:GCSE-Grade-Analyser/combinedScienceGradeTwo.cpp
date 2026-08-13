d30f13024fb748643c15c76b2d6aafd67b73f4ff
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 21:35:17 +0100

    Update combinedScienceGradeTwo.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/combinedScienceGradeTwo.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/combinedScienceGradeTwo.cpp
@@ -8,7 +8,7 @@ namespace secondScienceCombinedGrade {
   int combinedScienceGradeTwo() {
 
     cout << "Enter your second GCSE combined science grade?\n";
-    [[maybe_unused]] int scienceGradeTwo {};                                //This line is there to prevent the program from complaining that this variable isnt used if user decides to input 3 science grades instead of 2
+    [[maybe_unused]] static int scienceGradeTwo {};                                //This line is there to prevent the program from complaining that this variable isnt used if user decides to input 3 science grades instead of 2
     cin >> scienceGradeTwo;
     bool validScienceGradeTwo {scienceGradeTwo >= constants::minGrade && scienceGradeTwo <= constants::maxGrade};
     int finalScienceGradeTwo {(validScienceGradeTwo) ? scienceGradeTwo : 0};
