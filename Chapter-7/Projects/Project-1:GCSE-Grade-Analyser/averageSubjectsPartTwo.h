bee9f452b33dcbfb90f7d12991693555d462fecd
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 13:38:26 +0100

    Update averageSubjectsPartTwo.h

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSubjectsPartTwo.h
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSubjectsPartTwo.h
@@ -2,7 +2,7 @@
 #define AVERAGESUBJECTSPARTTWO
 
 namespace averageSubjectsTwo {
-  double averageSubjectsPartTwo(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalBiologyGrade, int finalChemistryGrade
+  double averageSubjectsPartTwo(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalBiologyGrade, int finalChemistryGrade,
                               int finalPhysicsGrade, int optionOneGrade, int optionTwoGrade, int optionThreeGrade, int optionFourGrade, int optionFiveGrade, int optionSixGrade);
 }
 
