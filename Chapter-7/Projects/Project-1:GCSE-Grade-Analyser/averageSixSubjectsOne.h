390fff57c36ccb73de66ece28a693efa10c45636
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:38:06 +0100

    Rename header guard and update function signature

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSixSubjectsOne.h
@@ -0,0 +1,8 @@
+#ifndef AVERAGESIXSUBJECTSPARTONE
+#define AVERAGEFIVESUBJECTSPARTONE
+
+namespace averageSixSubjectsOne {
+  double averageSixSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo, int optionOneGrade);
+}
+
+#endif
