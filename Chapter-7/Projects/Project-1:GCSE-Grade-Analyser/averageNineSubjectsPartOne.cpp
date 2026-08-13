7f1dae0334d7e9909ffed49be8f8c8ff1b5c7d31
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 21:31:35 +0100

    Update averageNineSubjectsPartOne.cpp

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageNineSubjectsPartOne.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageNineSubjectsPartOne.cpp
@@ -7,7 +7,7 @@ namespace averageNineSubjectsOne {
   double averageNineSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo
                                   , int optionOneGrade, int optionTwoGrade, int optionThreeGrade, int optionFourGrade) {
 
-    int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo
+    static int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo
                   + optionOneGrade + optionTwoGrade + optionThreeGrade + optionFourGrade};
     double average { static_cast<double>(totalScore) / static_cast<double>(finalCount) };
     return average;
