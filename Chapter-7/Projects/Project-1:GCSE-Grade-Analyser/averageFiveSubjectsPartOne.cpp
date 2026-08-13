8ab1d80553123c8e958a31c466ae3bfdf7772827
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:23:46 +0100

    Refactor averageFiveSubjectsPartOne into namespace

--- Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageFiveSubjectsPartOne.cpp
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageFiveSubjectsPartOne.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 #include "averageFiveSubjectsPartOne.h"
 
-double averageFiveSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo) {
+namespace averageFiveSubjectsOne {
+  double averageFiveSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo) {
 
-  int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo};
-  double averageFive { static_cast<double>(totalScore) / static_cast<double>(finalCount) };
-  return averageFive;
+    int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo};
+    double average { static_cast<double>(totalScore) / static_cast<double>(finalCount) };
+    return average;
+
+  }
 }
