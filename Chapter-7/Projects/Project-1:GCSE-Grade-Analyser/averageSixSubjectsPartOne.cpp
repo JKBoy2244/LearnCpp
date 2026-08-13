59fbd060ceb6decce6513678d243c8f52815ecdf
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:26:01 +0100

    Create averageSixSubjectsPartOne.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageSixSubjectsPartOne.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "averageSixSubjectsPartOne.h"
+
+namespace averageSixSubjectsOne {
+  double averageSixSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo, int optionOneGrade) {
+
+    int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo + optionOneGrade};
+    double average { static_cast<double>(totalScore) / static_cast<double>(finalCount) };
+    return average;
+  }
+}
