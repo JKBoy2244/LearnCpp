909e6d80ecb501c8843a35098dca4c7569daa340
JKBoy2244 <jobayer2006@gmail.com>
2026-08-13 12:28:45 +0100

    Create averageEightSubjectsPartOne.cpp

---
+++ Chapter-7/Projects/Project-1:GCSE-Grade-Analyser/averageEightSubjectsPartOne.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+#include "averageEightSubjectsPartOne.h"
+
+namespace averageEightSubjectsOne {
+  double averageEightSubjectsPartOne(int finalCount, int finalMathGrade, int finalEnglishLanguageGrade, int finalEnglishLiteratureGrade, int finalScienceGradeOne, int finalScienceGradeTwo
+                                   , int optionOneGrade, int optionTwoGrade, int optionThreeGrade) {
+
+    int totalScore {finalMathGrade + finalEnglishLanguageGrade + finalEnglishLiteratureGrade + finalScienceGradeOne + finalScienceGradeTwo + optionOneGrade + optionTwoGrade + optionThreeGrade};
+    double averageEight { static_cast<double>(totalScore) / static_cast<double>(finalCount) };
+    return averageEight;
+ }
+}
