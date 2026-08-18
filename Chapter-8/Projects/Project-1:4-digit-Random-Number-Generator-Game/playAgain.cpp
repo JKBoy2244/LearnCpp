1b289ee5f0421b1a26e809cf2f4792d49699739e
JKBoy2244 <jobayer2006@gmail.com>
2026-08-18 20:12:07 +0100

    Create playAgain.cpp

---
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/playAgain.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "playAgain.h"
+
+namespace Replay {
+  string playAgain() {
+
+    cout << "Do you want to play again or quit(yes = play again, no = quit) and please please type only yes or no all lowercase?\n";
+    string play {};
+    cin >> play;
+    return play;
+  }
+}
