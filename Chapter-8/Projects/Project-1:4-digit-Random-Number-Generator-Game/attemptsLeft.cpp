9165b157f2bbe52eebfadd44ed119aeff5cd6fb0
JKBoy2244 <jobayer2006@gmail.com>
2026-08-18 15:31:17 +0100

    Update attemptsLeft.cpp

--- Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/attemptsLeft.cpp
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/attemptsLeft.cpp
@@ -4,8 +4,8 @@ using namespace std;
 #include "constants.h"
 #include "attemptsLeft.h"
 
-namespace Attempts {
-  int attemptsLeft(constants::maxChances, int attempts) {
+namespace Attempts{
+  int attemptsLeft(int maxChances, int attempts) {
 
    int attemptsRemaining = constants::maxChances - attempts;
    return attemptsRemaining;
