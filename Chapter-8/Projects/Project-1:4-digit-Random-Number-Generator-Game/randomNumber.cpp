f081351f4d5ddd3680cdd96208556967cb055996
JKBoy2244 <jobayer2006@gmail.com>
2026-08-18 15:01:18 +0100

    Update randomNumber.cpp

--- Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/randomNumber.cpp
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/randomNumber.cpp
@@ -8,7 +8,7 @@ using namespace std;
 namespace Generate {
   int randomNumber() {
 
-     random_device rd;
+     random_device rd;     //this line is very important for seeding rd() to make the generation more random rather than some sequence
      mt19937 number {rd()};
      uniform_int_distribution range{constants::minNumber, constants::maxNumber};
      int programNumber = range(number);
