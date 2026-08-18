8cd1c58d3fe1c30367be7fbeb649f9f83f44b73f
JKBoy2244 <jobayer2006@gmail.com>
2026-08-18 19:48:14 +0100

    Create awardsClassiciation.cpp

---
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/awardsClassiciation.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "awardsClassiciation.h"
+
+namespace Award {
+  string_view awardsClassification(int points) {
+
+    if (points >= 880) {
+
+       string_view medal = "platinum";
+       return medal;
+    }
+
+    else if (points >= 680 && points < 880) {
+
+      string_view medal = "gold";
+      return medal;
+    }
+
+    else if (points >= 480 && points < 680) {
+
+      string_view medal = "silver";
+      return medal;
+    }
+
+    else if (points >= 180 && points < 480) {
+
+      string_view medal = "bronze";
+      return medal;
+    }
+
+    else {
+
+      string_view medal = "participation";
+      return medal;
+    }
+  }
+}
