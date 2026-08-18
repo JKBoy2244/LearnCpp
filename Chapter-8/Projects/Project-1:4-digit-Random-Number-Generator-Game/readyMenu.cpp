6803d492ca652860210c86836ca17057c6e050c6
JKBoy2244 <jobayer2006@gmail.com>
2026-08-18 13:48:10 +0100

    Update readyMenu.cpp

--- Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/readyMenu.cpp
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/readyMenu.cpp
@@ -9,7 +9,7 @@ namespace Menu {
     cout << "Welcome to the 4 digit random number generator game. This game you will have 20 chances to guess the number I am thinking off and along the way we give hints but at maximum 5 along the way\n";
     cout << "You start with 1000 points but every time you get a guess wrong which is extremely likely at least, you lose 40 points each time\n";
     cout << "But, don't worry because this is meant to be a deliberately challenging game which is the point\n";
-    cout << "Are you ready to play though (please type 'y' or 'n' and lowercase specific);
+    cout << "Are you ready to play though (please type 'y' or 'n' and lowercase specific)\n";
     char ready {};
     cin >> ready;
     return ready;
