20ce97b82a4d2ee4b7bff7444f1ee69241471ca1
JKBoy2244 <jobayer2006@gmail.com>
2026-08-18 22:20:20 +0100

    Update finalResult.cpp

--- Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/finalResult.cpp
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/finalResult.cpp
@@ -8,6 +8,6 @@ inline namespace Result {
   void finalResult(int points, string_view medal) {
 
     cout << "You scored " << points << " this round so hence you receive the " << medal << " award!\n";
-    cout << "Boundaries: 880 points = platinum, 680 points = gold, 480 points = silver, 300 points = bronze and < 180 = participation!\n";
+    cout << "Boundaries: 880 points = platinum, 680 points = gold, 480 points = silver, 300 points = bronze and < 300 = participation!\n";
   }
 }
