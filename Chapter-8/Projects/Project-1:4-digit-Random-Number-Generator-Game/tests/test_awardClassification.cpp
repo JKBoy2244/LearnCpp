387eead079a0a598198926d9d107a75d1df3f74e
JKBoy2244 <jobayer2006@gmail.com>
2026-09-12 11:33:10 +0100

    Refactor award classification tests to use loop
    Refactored award classification tests to use a loop for better maintainability and coverage.

--- Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/tests/test_awardClassification.cpp
+++ Chapter-8/Projects/Project-1:4-digit-Random-Number-Generator-Game/tests/test_awardClassification.cpp
@@ -5,17 +5,15 @@
 
 int main() {
 
-    assert(Award::awardsClassification(1000) == "platinum");
-    assert(Award::awardsClassification(900) == "platinum");
-    assert(Award::awardsClassification(800) == "platinum");
-    assert(Award::awardsClassification(700) == "platinum");
-    assert(Award::awardsClassification(600) == "platinum");
-    assert(Award::awardsClassification(500) == "platinum");
-    assert(Award::awardsClassification(400) == "platinum");
-    assert(Award::awardsClassification(300) == "platinum");
-    assert(Award::awardsClassification(200) == "platinum");
-    assert(Award::awardsClassification(100) == "platinum");
-    assert(Award::awardsClassification(0) == "platinum");
+    for (int awardTest {1000}; awardTest >= 0; --awardTest) 
+    {
+
+        assert(Award::awardsClassification( (awardTest >= 880) && (awardTest <= 1000) ) == "platinum");
+        assert(Award::awardsClassification( (awardTest >= 680) && (awardTest <= 879) ) == "gold");
+        assert(Award::awardsClassification( (awardTest >= 480) && (awardTest <= 679) ) == "silver");
+        assert(Award::awardsClassification( (awardTest >= 300) && (awardTest <= 479 )) == "bronze");
+        assert(Award::awardsClassification( (awardTest < 300) == "participation");
+   }
 
     std::cout << "All award classification tests passed!";
 }
